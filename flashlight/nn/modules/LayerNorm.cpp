7744c012afe1092ed3516b8807d64fd5fc395d04
Vineel Pratap <vineelkpratap@fb.com>
2019-12-20 15:45:45 -0800

    Revert LN to use cudnn call
    Summary:
    While there is huge improvement across different GPU with different input sizes when we use Arrayfire ops to do LayerNorm instead if cudnn call, I'm see some regression while training w2l models with the new implementation.
    
    {F225356834}
    
    Would like to revert back to original version until I found the root cause. Unfortunately, the code is ugly because, we need to support non-contiguous normalization axes.
    
    Reviewed By: tlikhomanenko
    
    Differential Revision: D19192285
    
    fbshipit-source-id: d5de674d11b1b662d0ffc4c62ee829d5e9ad7252

--- flashlight/nn/modules/LayerNorm.cpp
+++ flashlight/nn/modules/LayerNorm.cpp
@@ -39,33 +39,75 @@ LayerNorm::LayerNorm(
 }
 
 Variable LayerNorm::forward(const Variable& input) {
-  std::vector<int> axis;
-  for (int d = 0; d < AF_MAX_DIMS; ++d) {
-    if (std::find(axisComplement_.begin(), axisComplement_.end(), d) ==
-        axisComplement_.end()) {
-      axis.push_back(d);
+  Variable dummyInMean, dummyInVar;
+
+  auto weight = Variable();
+  auto bias = Variable();
+
+  if (affine_ && axisSize_ == kLnVariableAxisSize) {
+    af::dim4 tiledims(1, 1, 1, 1);
+    for (int ax : axisComplement_) {
+      tiledims[ax] = input.dims(ax);
     }
+    weight = tile(params_[0], tiledims);
+    bias = tile(params_[1], tiledims);
+  } else if (affine_) {
+    weight = params_[0];
+    bias = params_[1];
   }
 
-  auto mean = fl::tileAs(fl::mean(input, axis), input);
-  auto stddev =
-      fl::tileAs(fl::sqrt(fl::var(input, axis, true) + epsilon_), input);
-
-  auto output = (input - mean) / stddev;
-
-  if (!affine_) {
-    return output;
-  }
-  Variable weight = params_[0], bias = params_[1];
-  if (axisSize_ != kLnVariableAxisSize) {
-    af::dim4 featDims(1, 1, 1, 1);
-    for (auto i : axis) {
-      featDims[i] = input.dims(i);
+  Variable inputToBn = input;
+  std::vector<int> inNormAxes;
+  // reorder is only required if axisComplement_ is not continuous
+  std::array<int, AF_MAX_DIMS> reorderDims;
+  auto maxAxis =
+      *std::max_element(axisComplement_.begin(), axisComplement_.end());
+  auto minAxis =
+      *std::min_element(axisComplement_.begin(), axisComplement_.end());
+  bool axesContinuous = (axisComplement_.size() == (maxAxis - minAxis + 1));
+  if (axesContinuous) {
+    inNormAxes = axisComplement_;
+  } else {
+    int i = 0;
+    for (int d = 0; d < AF_MAX_DIMS; ++d) {
+      if (std::find(axisComplement_.begin(), axisComplement_.end(), d) ==
+          axisComplement_.end()) {
+        inNormAxes.push_back(i);
+        reorderDims[i++] = d;
+      }
     }
-    weight = fl::moddims(params_[0], featDims);
-    bias = fl::moddims(params_[1], featDims);
+    for (auto n : axisComplement_) {
+      reorderDims[i++] = n;
+    }
+    inputToBn = reorder(
+        input, reorderDims[0], reorderDims[1], reorderDims[2], reorderDims[3]);
   }
-  return tileAs(weight, input) * output + tileAs(bias, input);
+
+  auto output = batchnorm(
+      inputToBn,
+      weight,
+      bias,
+      dummyInMean,
+      dummyInVar,
+      inNormAxes,
+      true,
+      0.0,
+      epsilon_);
+
+  if (!axesContinuous) {
+    std::vector<std::pair<int, int>> restoreDims = {{reorderDims[0], 0},
+                                                    {reorderDims[1], 1},
+                                                    {reorderDims[2], 2},
+                                                    {reorderDims[3], 3}};
+    std::sort(restoreDims.begin(), restoreDims.end());
+    output = reorder(
+        output,
+        restoreDims[0].second,
+        restoreDims[1].second,
+        restoreDims[2].second,
+        restoreDims[3].second);
+  }
+  return output;
 }
 
 void LayerNorm::initialize() {
