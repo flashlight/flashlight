49a3c8ed6fa0b6cdcd9f098b8d78d5cc76192039
Tatiana Likhomanenko <antares@fb.com>
2020-01-12 12:11:34 -0800

    add copyright/license to pos emb
    Summary: title
    
    Reviewed By: jacobkahn
    
    Differential Revision: D19368240
    
    fbshipit-source-id: e1938ca9aaa5beec2887be602b87c1634f585f71

--- flashlight/contrib/modules/PositionEmbedding.cpp
+++ flashlight/contrib/modules/PositionEmbedding.cpp
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include "flashlight/contrib/modules/PositionEmbedding.h"
 
 #include "flashlight/autograd/Functions.h"
