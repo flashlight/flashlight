dea832219394440a6763049a5b16e0455ad8c2ad
Paden Tomasello <padentomasello@devfair049.maas>
2020-06-10 11:33:19 -0700

    Save backbone model to different path

--- wrapper.sh
+++ wrapper.sh
@@ -14,6 +14,6 @@ echo $SLURM_NTASKS
 echo $SLURM_LOCALID
 BUILD_DIR=/scratch/slurm_tmpdir/$SLURM_JOB_ID/$1
 mkdir -p $BUILD_DIR/rndv/
-AF_MAX_BUFFERS=3000 $BUILD_DIR/flashlight/build/vision/Resnet34Backbone --world_rank $SLURM_LOCALID --world_size $SLURM_NTASKS --rndv_filepath $BUILD_DIR/rndv --checkpointpath /checkpoint/padentomasello/models/resnet34
+AF_MAX_BUFFERS=3000 $BUILD_DIR/flashlight/build/vision/Resnet34Backbone --world_rank $SLURM_LOCALID --world_size $SLURM_NTASKS --rndv_filepath $BUILD_DIR/rndv --checkpointpath /checkpoint/padentomasello/models/resnet34backbone
 
 # Your CUDA enabled program here
