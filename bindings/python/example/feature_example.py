f725a0d53026834106244463957f253c17380022
Tatiana Likhomanenko <antares@fb.com>
2020-09-04 11:02:59 -0700

    fix license for the python bindings
    Summary: title
    
    Reviewed By: andresy
    
    Differential Revision: D23542575
    
    fbshipit-source-id: 4993bf250729fadaa1b6fbc893f7fe11a7ce2364

--- bindings/python/example/feature_example.py
+++ bindings/python/example/feature_example.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-
-# adapted from wav2letter/src/feature/test/MfccTest.cpp
+"""
+Copyright (c) Facebook, Inc. and its affiliates.
+All rights reserved.
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+"""
 
 import itertools as it
 import os
