d5defefc500b9b89d3172fa05cd31c390dc3a4b8
Vineel Pratap <vineelkpratap@fb.com>
2020-09-18 12:32:43 -0700

    Dataset Cleanup [Part 4/N] (#168)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/168
    
    Adds `createDataset` which provides an easy way to create a list file dataset.
    
    Reviewed By: tlikhomanenko
    
    Differential Revision: D23738820
    
    fbshipit-source-id: 76d3a06d0be61117303f83c71620576e217d0bc4

--- app/asr/runtime/Helpers.cpp
+++ app/asr/runtime/Helpers.cpp
@@ -167,6 +167,88 @@ std::shared_ptr<Dataset> createDataset(
 
   return ds;
 }
+
+std::shared_ptr<fl::Dataset> createDataset(
+    const std::vector<std::string>& paths,
+    const std::string& rootDir /* = "" */,
+    int batchSize /* = 1 */,
+    const fl::Dataset::DataTransformFunction& inputTransform /* = nullptr */,
+    const fl::Dataset::DataTransformFunction& targetTransform /* = nullptr */,
+    const fl::Dataset::DataTransformFunction& wordTransform /* = nullptr */,
+    const std::tuple<int, int, int>& padVal /* = {0, -1, -1} */,
+    int worldRank /* = 0 */,
+    int worldSize /* = 1 */) {
+  std::vector<std::shared_ptr<const fl::Dataset>> allListDs;
+  std::vector<float> sizes;
+  for (auto& path : paths) {
+    auto curListDs = std::make_shared<ListFileDataset>(
+        pathsConcat(rootDir, path),
+        inputTransform,
+        targetTransform,
+        wordTransform);
+
+    allListDs.emplace_back(curListDs);
+    sizes.reserve(sizes.size() + curListDs->size());
+    for (int64_t i = 0; i < curListDs->size(); ++i) {
+      sizes.push_back(curListDs->getInputSize(i));
+    }
+  }
+
+  // Order Dataset
+  std::vector<int64_t> sortedIds(sizes.size());
+  std::iota(sortedIds.begin(), sortedIds.end(), 0);
+  auto cmp = [&sizes](const int64_t& l, const int64_t& r) {
+    return sizes[l] < sizes[r];
+  };
+  std::stable_sort(sortedIds.begin(), sortedIds.end(), cmp);
+
+  auto concatListDs = std::make_shared<fl::ConcatDataset>(allListDs);
+
+  auto sortedDs =
+      std::make_shared<fl::ResampleDataset>(concatListDs, sortedIds);
+
+  // Partition the dataset and distribute
+  auto partitions = fl::partitionByRoundRobin(
+      sortedDs->size(), worldRank, worldSize, batchSize);
+  auto paritionDs = std::make_shared<fl::ResampleDataset>(sortedDs, partitions);
+
+  // Batch the dataset
+  int inPad, tgtPad, wrdPad;
+  std::tie(inPad, tgtPad, wrdPad) = padVal;
+  return std::make_shared<fl::BatchDataset>(
+      paritionDs,
+      batchSize,
+      fl::BatchDatasetPolicy::INCLUDE_LAST,
+      std::vector<fl::Dataset::BatchFunction>{
+          [inPad](const std::vector<af::array>& arr) {
+            return fl::join(arr, inPad, 3);
+          },
+          [tgtPad](const std::vector<af::array>& arr) {
+            return fl::join(arr, tgtPad, 1);
+          },
+          [wrdPad](const std::vector<af::array>& arr) {
+            return fl::join(arr, wrdPad, 1);
+          },
+          [](const std::vector<af::array>& arr) {
+            return fl::join(arr, 0, 1);
+          }});
+}
+
+std::shared_ptr<fl::Dataset> loadPrefetchDataset(
+    std::shared_ptr<fl::Dataset> dataset,
+    int prefetchThreads,
+    bool shuffle,
+    int shuffleSeed /*= 0 */) {
+  if (shuffle) {
+    dataset = std::make_shared<fl::ShuffleDataset>(dataset, shuffleSeed);
+  }
+  if (prefetchThreads > 0) {
+    dataset = std::make_shared<fl::PrefetchDataset>(
+        dataset, prefetchThreads, prefetchThreads /* prefetch size */);
+  }
+  return dataset;
+}
+
 } // namespace asr
 } // namespace app
 } // namespace fl
