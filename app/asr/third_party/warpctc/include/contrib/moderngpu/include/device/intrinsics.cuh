f47f576cba6e39b68291ddad7507f9a07b3934df
Jacob Kahn <jacobkahn@fb.com>
2020-10-01 16:34:16 -0700

    Fix copyright header pollution in doxygen (#189)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/189
    
    tl;dr, just some perl
    ```
    find . -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.cuh" -o -name "*.cu"  -o -name "*.hpp" \) | xargs perl -i -p0e 's/\*\*\n \* Copyright/`cat test.correct.2.txt`/se'
    ```
    
    When we generate docs, doxygen doesn't know to ignore copyright headers since they start with `/**`. This creates unreadable garbage like https://fl.readthedocs.io/en/latest/serial.html?highlight=Copyright.
    
    Fix this by starting copyright headers with `/*` so doxygen doesn't include them in docs generation
    
    Reviewed By: tlikhomanenko
    
    Differential Revision: D24022206
    
    fbshipit-source-id: 93d5a617776ff0e45ec3d9b32a55d45c90cb1d83

--- app/asr/third_party/warpctc/include/contrib/moderngpu/include/device/intrinsics.cuh
+++ app/asr/third_party/warpctc/include/contrib/moderngpu/include/device/intrinsics.cuh
@@ -1,4 +1,4 @@
-/******************************************************************************
+/*****************************************************************************
  * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
--- flashlight/dataset/ShuffleDataset.cpp
+++ flashlight/dataset/ShuffleDataset.cpp
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the BSD-style license found in the
