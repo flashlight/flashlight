09c51e46434f61b464f12a0d08f8e1da6c238126
Jacob Kahn <jacobkahn@fb.com>
2020-11-03 23:29:52 -0800

    Fix header install paths, globbing, exclusion, and config params (#233)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/233
    
    Clean up which headers we install.
    
    We use `install(DIRECTORY ...)` with `FILES_MATCHING`, which is the easiest way to recursively move headers in CMake.
    
    The problem is (yep, this is real) that empty directories that don't have matching patterns are still installed. [There's a ticket for this](https://gitlab.kitware.com/cmake/cmake/-/issues/17122) but lord knows when it'll be merged in. Either way, we need to support old behaviors.
    
    Adds exclusive patterns for things we don't want to install to avoid having empty directories (vcpkg also prohibits having empty directories).
    
    This patterns are a little scary (reasonable so far) -- if we need to add more things that don't fit with these patterns well, we can modify the vcpkg port to manually remove the directory.
    
    The only alternative here is to use CMake's `file(GLOB_RECURSE ...)` to manually locate headers, but this doesn't preserve relative paths and creates a ton of headaches.
    
    Reviewed By: tlikhomanenko
    
    Differential Revision: D24720661
    
    fbshipit-source-id: c73ef1b6fba653fded4d175572a50b0189d561db

--- cmake/InternalUtils.cmake
+++ cmake/InternalUtils.cmake
@@ -3,7 +3,7 @@ function(setup_install_targets)
   set(multiValueArgs INSTALL_TARGETS INSTALL_HEADERS)
   cmake_parse_arguments(setup_install_targets "${options}" "${oneValueArgs}"
     "${multiValueArgs}" ${ARGN})
-    
+
   # Main target
   install(
     TARGETS ${setup_install_targets_INSTALL_TARGETS}
@@ -16,17 +16,13 @@ function(setup_install_targets)
     FRAMEWORK DESTINATION framework
     INCLUDES DESTINATION ${FL_INSTALL_INC_DIR}
     )
-  
+
   # Write and install targets file
   install(
-    EXPORT
-    flashlightTargets
-    NAMESPACE
-    flashlight::
-    DESTINATION
-    ${FL_INSTALL_CMAKE_DIR}
-    COMPONENT
-    cmake
+    EXPORT flashlightTargets
+    NAMESPACE flashlight::
+    DESTINATION ${FL_INSTALL_CMAKE_DIR}
+    COMPONENT flashlight
     )
 
   # Write config file (used by projects including fl, such as examples)
@@ -43,21 +39,24 @@ function(setup_install_targets)
   install(FILES
     ${PROJECT_BINARY_DIR}/cmake/install/flashlightConfig.cmake
     DESTINATION ${FL_INSTALL_CMAKE_DIR}
-    COMPONENT cmake
+    COMPONENT flashlight
     )
 endfunction(setup_install_targets)
 
 function(setup_install_headers HEADER_DIR DEST_DIR)
   # Move headers
   install(
-    DIRECTORY
-    ${HEADER_DIR}
-    COMPONENT
-    headers
-    DESTINATION
-    ${DEST_DIR}
+    DIRECTORY ${HEADER_DIR}
+    COMPONENT headers
+    DESTINATION ${DEST_DIR}
     FILES_MATCHING # preserve directory structure
     PATTERN  "*.h"
+    PATTERN "*.cuh" # TODO: gate me
+    PATTERN "test*" EXCLUDE
     PATTERN "tests" EXCLUDE
+    PATTERN "backend" EXCLUDE
+    PATTERN "examples" EXCLUDE
+    PATTERN "third_party" EXCLUDE
+    PATTERN "experimental" EXCLUDE
     )
 endfunction(setup_install_headers)
