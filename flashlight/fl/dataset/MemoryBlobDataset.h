d1bf7fb5386097e5cc959dc56c0a0b59f6c4e757
Tatiana Likhomanenko <antares@fb.com>
2020-11-06 14:03:55 -0800

    MemoryBlobDataset (#245)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/245
    
    MemoryBlobDataset authored by andresy
    
    Reviewed By: jacobkahn
    
    Differential Revision: D24520329
    
    fbshipit-source-id: 38887f5bbdf85f6f65b624c656c8a74a94043bb6

---
+++ flashlight/fl/dataset/MemoryBlobDataset.h
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "flashlight/fl/dataset/BlobDataset.h"
+
+#include <fstream>
+#include <mutex>
+
+namespace fl {
+
+/**
+ * A BlobDataset in (CPU) memory.
+ *
+ * As the arrays are stored on disk, sequential access will be the most
+ * efficient.
+ *
+ */
+class MemoryBlobDataset : public BlobDataset {
+ public:
+  /**
+   * Creates a `MemoryBlobDataset`, specifying a blob file name.
+   */
+  MemoryBlobDataset();
+
+  virtual ~MemoryBlobDataset() override = default;
+
+ protected:
+  int64_t writeData(int64_t offset, const char* data, int64_t size)
+      const override;
+  int64_t readData(int64_t offset, char* data, int64_t size) const override;
+  void flushData() override;
+  bool isEmptyData() const override;
+
+ private:
+  mutable std::mutex writeMutex_;
+  mutable std::vector<char> data_;
+};
+
+} // namespace fl
