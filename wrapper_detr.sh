66d24bd82f91a77a6c606e6116e396bc4a2a1023
Paden Tomasello <padentomasello@devfair049.maas>
2020-11-11 17:38:25 -0800

    Remove flags

--- wrapper_detr.sh
+++ wrapper_detr.sh
@@ -25,8 +25,7 @@ $BUILD_DIR/flashlight/build/Detr -lr 0.0001 --epochs 500 --batch_size 4 \
 --rndv_filepath $BUILD_DIR/rndv/ \
 --checkpointpath /checkpoint/padentomasello/models/detr \
 --eval_dir $EVAL_DIR \
---tryfromenv=eval_iters,data_dir,metric_iters,pretrained,print_params \
---noequal_aspect --noonesize
+--tryfromenv=eval_iters,data_dir,metric_iters,pretrained,print_params 
 
 
 
