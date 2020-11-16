7990a3d538bdf814a0a34b9c8e62384d1ee75e5e
Jacob Kahn <jacobkahn@fb.com>
2020-11-16 11:23:49 -0800

    Fix target names in package modules (#263)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/263
    
    Package names have to batch the targets being searched for in order for `find_dependencies` calls to work in the standalone install. This is the case with newer versions of CMake.
    
    Reviewed By: tlikhomanenko
    
    Differential Revision: D24993902
    
    fbshipit-source-id: 93524109a76b3926fc04db891041e96da1764e94

--- cmake/FindSndFile.cmake
+++ cmake/FindSndFile.cmake
@@ -4,14 +4,14 @@
 # - SndFile::sndfile
 #
 # Inputs:
-#   SNDFILE_INC_DIR: include directory for sndfile headers
-#   SNDFILE_LIB_DIR: directory containing sndfile libraries
-#   SNDFILE_ROOT_DIR: directory containing sndfile installation
+#   SndFile_INC_DIR: include directory for sndfile headers
+#   SndFile_LIB_DIR: directory containing sndfile libraries
+#   SndFile_ROOT_DIR: directory containing sndfile installation
 #
 # Defines:
-#  SNDFILE_FOUND - system has libsndfile
-#  SNDFILE_INCLUDE_DIRS - the libsndfile include directory
-#  SNDFILE_LIBRARIES - Link these to use libsndfile
+#  SndFile_FOUND - system has libsndfile
+#  SndFile_INCLUDE_DIRS - the libsndfile include directory
+#  SndFile_LIBRARIES - Link these to use libsndfile
 #
 
 # We require sndfile having been built with external libs.
@@ -22,47 +22,47 @@ find_package(FLAC REQUIRED)
 find_package(SndFile CONFIG)
 
 if (TARGET SndFile::sndfile AND NOT SndFile_WITH_EXTERNAL_LIBS)
-  message(WARNING "Found sndfile but was NOT built with external libs.")
+  message(FATAL_ERROR "Found sndfile but was NOT built with external libs.")
 endif()
 
 if (NOT TARGET SndFile::sndfile)
   find_path(
-    SNDFILE_INCLUDE_DIR
+    SndFile_INCLUDE_DIR
       sndfile.h
     PATHS
-    ${SNDFILE_INC_DIR}
-    ${SNDFILE_ROOT_DIR}/include
+    ${SndFile_INC_DIR}
+    ${SndFile_ROOT_DIR}/include
     PATH_SUFFIXES
     include
     )
 
   find_library(
-    SNDFILE_LIBRARY
+    SndFile_LIBRARY
     sndfile
     PATHS
-    ${SNDFILE_LIB_DIR}
-    ${SNDFILE_ROOT_DIR}
+    ${SndFile_LIB_DIR}
+    ${SndFile_ROOT_DIR}
     PATH_SUFFIXES
     lib
     HINTS
     SNDFILE
     )
 
-  set(SNDFILE_INCLUDE_DIRS
-    ${SNDFILE_INCLUDE_DIR}
+  set(SndFile_INCLUDE_DIRS
+    ${SndFile_INCLUDE_DIR}
     )
-  set(SNDFILE_LIBRARIES
-    ${SNDFILE_LIBRARY}
+  set(SndFile_LIBRARIES
+    ${SndFile_LIBRARY}
     )
 
-  mark_as_advanced(SNDFILE_INCLUDE_DIRS SNDFILE_LIBRARIES)
+  mark_as_advanced(SndFile_INCLUDE_DIRS SndFile_LIBRARIES)
   include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(SNDFILE DEFAULT_MSG SNDFILE_INCLUDE_DIRS GLOG_LIBRARIES)
+  find_package_handle_standard_args(SndFile DEFAULT_MSG SndFile_INCLUDE_DIRS SndFile_LIBRARIES)
 
   add_library(SndFile::sndfile UNKNOWN IMPORTED)
   set_target_properties(SndFile::sndfile PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${SNDFILE_INCLUDE_DIRS}"
-    IMPORTED_LOCATION "${SNDFILE_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SndFile_INCLUDE_DIRS}"
+    IMPORTED_LOCATION "${SndFile_LIBRARIES}"
     )
   set_property(
     TARGET SndFile::sndfile
@@ -73,8 +73,8 @@ if (NOT TARGET SndFile::sndfile)
     FLAC::FLAC
     )
 
-  if (SNDFILE_FOUND)
-    message(STATUS "Found libsndfile: (lib: ${SNDFILE_LIBRARIES} include: ${SNDFILE_INCLUDE_DIRS}")
+  if (SndFile_FOUND)
+    message(STATUS "Found libsndfile: (lib: ${SndFile_LIBRARIES} include: ${SndFile_INCLUDE_DIRS}")
   else()
     message(STATUS "libsndfile not found.")
   endif()
