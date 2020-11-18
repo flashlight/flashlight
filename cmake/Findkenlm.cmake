c37cf60141fa674b558f622c0a65853461f7a614
Jacob Kahn <jacobkahn@fb.com>
2020-11-17 22:38:56 -0800

    Change KENLM_ROOT_DIR to KENLM_ROOT
    Summary: Changed to be consistent with other environment variables (e.g. `MKLROOT`, etc)
    
    Differential Revision: D25047794
    
    fbshipit-source-id: a02f00dd6e406b3e22240dfecd16236e52f7de4b

--- cmake/Findkenlm.cmake
+++ cmake/Findkenlm.cmake
@@ -1,7 +1,7 @@
 # Try to find the KenLM library
 #
 # The following variables are optionally searched for defaults
-#  KENLM_ROOT_DIR: Base directory where all KENLM components are found
+#  KENLM_ROOT: Base directory where all KENLM components are found
 #
 # The following are set after configuration is done:
 #  KENLM_FOUND
@@ -15,34 +15,34 @@ find_library(
   KENLM_LIB
   kenlm
   HINTS
-    ${KENLM_ROOT_DIR}/lib
-    ${KENLM_ROOT_DIR}/build/lib
+    ${KENLM_ROOT}/lib
+    ${KENLM_ROOT}/build/lib
   PATHS
-    $ENV{KENLM_ROOT_DIR}/lib
-    $ENV{KENLM_ROOT_DIR}/build/lib
+    $ENV{KENLM_ROOT}/lib
+    $ENV{KENLM_ROOT}/build/lib
   )
 
 find_library(
   KENLM_UTIL_LIB
   kenlm_util
   HINTS
-    ${KENLM_ROOT_DIR}/lib
-    ${KENLM_ROOT_DIR}/build/lib
+    ${KENLM_ROOT}/lib
+    ${KENLM_ROOT}/build/lib
   PATHS
-    $ENV{KENLM_ROOT_DIR}/lib
-    $ENV{KENLM_ROOT_DIR}/build/lib
+    $ENV{KENLM_ROOT}/lib
+    $ENV{KENLM_ROOT}/build/lib
   )
 
 if(KENLM_LIB)
   message(STATUS "Using kenlm library found in ${KENLM_LIB}")
 else()
-  message(FATAL_ERROR "kenlm library not found; please set CMAKE_LIBRARY_PATH, KENLM_LIB or KENLM_ROOT_DIR environment variable")
+  message(FATAL_ERROR "kenlm library not found; please set CMAKE_LIBRARY_PATH, KENLM_LIB or KENLM_ROOT environment variable")
 endif()
 
 if(KENLM_UTIL_LIB)
   message(STATUS "Using kenlm utils library found in ${KENLM_UTIL_LIB}")
 else()
-  message(FATAL_ERROR "kenlm utils library not found; please set CMAKE_LIBRARY_PATH, KENLM_UTIL_LIB or KENLM_ROOT_DIR environment variable")
+  message(FATAL_ERROR "kenlm utils library not found; please set CMAKE_LIBRARY_PATH, KENLM_UTIL_LIB or KENLM_ROOT environment variable")
 endif()
 
 # find a model header, then get the entire include directory. We need to do this because
@@ -50,17 +50,17 @@ endif()
 find_file(KENLM_MODEL_HEADER
   model.hh
   HINTS
-    ${KENLM_ROOT_DIR}/lm
-    ${KENLM_ROOT_DIR}/include/kenlm/lm
+    ${KENLM_ROOT}/lm
+    ${KENLM_ROOT}/include/kenlm/lm
   PATHS
-    $ENV{KENLM_ROOT_DIR}/lm
-    $ENV{KENLM_ROOT_DIR}/include/kenlm/lm
+    $ENV{KENLM_ROOT}/lm
+    $ENV{KENLM_ROOT}/include/kenlm/lm
   )
 
 if(KENLM_MODEL_HEADER)
   message(STATUS "kenlm model.hh found in ${KENLM_MODEL_HEADER}")
 else()
-  message(FATAL_ERROR "kenlm model.hh not found; please set CMAKE_INCLUDE_PATH, KENLM_MODEL_HEADER or KENLM_ROOT_DIR environment variable")
+  message(FATAL_ERROR "kenlm model.hh not found; please set CMAKE_INCLUDE_PATH, KENLM_MODEL_HEADER or KENLM_ROOT environment variable")
 endif()
 get_filename_component(KENLM_INCLUDE_LM ${KENLM_MODEL_HEADER} DIRECTORY)
 get_filename_component(KENLM_INCLUDE_DIR ${KENLM_INCLUDE_LM} DIRECTORY)
@@ -73,5 +73,5 @@ find_package_handle_standard_args(kenlm DEFAULT_MSG KENLM_INCLUDE_DIRS KENLM_LIB
 
 if (kenlm_FOUND)
   message(STATUS "Found kenlm (include: ${KENLM_INCLUDE_DIRS}, library: ${KENLM_LIBRARIES})")
-  mark_as_advanced(KENLM_ROOT_DIR KENLM_INCLUDE_DIRS KENLM_LIBRARIES)
+  mark_as_advanced(KENLM_ROOT KENLM_INCLUDE_DIRS KENLM_LIBRARIES)
 endif()
