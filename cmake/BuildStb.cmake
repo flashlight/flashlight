5f9675a7d3d7ee8e85ef1ec4c3cc788f6825a14a
Paden Tomasello <padentomasello@devfair049.maas>
2020-11-18 17:29:52 -0800

    Remove stb_image.h - checkout stb repository using cmake (#232)
    Summary:
    **IMPORTANT: Please do not create a Pull Request without creating an issue first.** Changes *must* be discussed.
    
    **Original Issue**: [corresponding issue on Github]
    
    *Note:* You can add `closes #[issue number]` to automatically close the issue that this PR resolves when it is merged.
    
    [Explain the details for this change and the problem that the pull request solves]
    
    ### Test Plan (required)
    [steps by which you tested that your fix resolves the issue. These might include specific commands and configurations]
    
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/232
    
    Test Plan:
    On FB Cluster
    
    `buck run mode/opt deeplearning/projects/flashlight/flashlight/app/imclass/examples:imageNetResnet34`
    
    On FAIR Cluster
    
    `make -j imageNetResnet34`
    
    Reviewed By: jacobkahn
    
    Differential Revision: D24712181
    
    Pulled By: padentomasello
    
    fbshipit-source-id: 5d85d8e01d0d713fc2c1ab0e8b8392d78eefc61a

---
+++ cmake/BuildStb.cmake
@@ -0,0 +1,26 @@
+include(ExternalProject)
+
+set(stb_URL https://github.com/nothings/stb.git)
+set(stb_BUILD ${CMAKE_CURRENT_BINARY_DIR}/stb)
+set(stb_TAG b42009b3b9d4ca35bc703f5310eedc74f584be58)
+
+# Download stb
+ExternalProject_Add(
+  stb
+  PREFIX stb
+  GIT_REPOSITORY ${stb_URL}
+  GIT_TAG ${stb_TAG}
+  BUILD_IN_SOURCE 0
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  LOG_DOWNLOAD ON
+)
+
+ExternalProject_Get_Property(stb SOURCE_DIR)
+set(STB_SOURCE_DIR ${SOURCE_DIR})
+
+set(stb_INCLUDE_DIRS
+  $<BUILD_INTERFACE:${SOURCE_DIR}/>
+  $<INSTALL_INTERFACE:${STB_INSTALL_PATH}>
+  )
