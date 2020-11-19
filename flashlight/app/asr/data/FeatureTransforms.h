flashlight/flashlight

commit e83e7b6720f9407bc706d6c26f0a599dc0941285

improve target preparation for speech (#241)
Summary:
- Before word separator is inserted between words transcriptions and specifically duplication of it is removed (in case of word pieces, for example it was _hello _ _world -> _hello _world). However, this doesn't work correctly for separate word pieces, like "hello world" with letters will be mapped "h e l l o | w o r l d" while it should be "h e l l o | w o r l d |".
- Another error occurs if unk (not in the lexicon) word is the first or last in the transcription, here for letters the last word is mapped wrongly and the first word for word pieces is mapped wrongly too.

This huge problem is fixed in this PR:
- we rely on lexicon to resolve all word separator correct place
- for unk words we rely on fallback and specify where word separator should be added (emulate lexicon), left, right or both sides. User defines how unk fallback should be processed with word separator.
- add surround (if it is specified) at the begin and end if it is different from word separator, otherwise if word separator == surround, add surround only if fist/last token != surround.

### Test Plan (required)
- added four use cases of lexicon/tokens/fallback for separate word separator and the case when it is inside a word.

Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/241

Reviewed By: vineelpratap

Differential Revision: D24723972

Pulled By: tlikhomanenko

fbshipit-source-id: bb7314ba00d6ee5a9b1e58012e9be32c567c0d18

flashlight/app/asr/data/FeatureTransforms.h
@@ -29,15 +29,16 @@ struct TargetGenerationConfig {
       bool eosToken,
       int replabel,
       bool skipUnk,
-      bool fallbackToLetter)
-      : wordSeparator_(wordSeparator),
-        targetSamplePct_(targetSamplePct),
+      bool fallbackToLetterWordSepLeft,
+      bool fallbackToLetterWordSepRight)
+      : targetSamplePct_(targetSamplePct),
         criterion_(criterion),
         surround_(surround),
         eosToken_(eosToken),
         replabel_(replabel),
         skipUnk_(skipUnk),
-        fallbackToLetter_(fallbackToLetter) {}
+        fallbackToLetterWordSepLeft_(fallbackToLetterWordSepLeft),
+        fallbackToLetterWordSepRight_(fallbackToLetterWordSepRight) {}
 
   // token separator between words
   const std::string wordSeparator_;
@@ -53,8 +54,12 @@ struct TargetGenerationConfig {
   const int replabel_;
   // skip unknown tokens
   const bool skipUnk_;
-  // use letters of word as tokens if a word is not present in lexicon
-  const bool fallbackToLetter_;
+  // use letters of word as tokens if a word is not present in lexicon 
+  // + add wordseparator at the beginning
+  const bool fallbackToLetterWordSepLeft_;
+  // use letters of word as tokens if a word is not present in lexicon 
+  // + add wordseparator at the end
+  const bool fallbackToLetterWordSepRight_;      
 };
 
 fl::Dataset::DataTransformFunction inputFeatures(

flashlight/app/asr/Train.cpp
@@ -269,7 +269,8 @@ int main(int argc, char** argv) {
       FLAGS_eostoken,
       FLAGS_replabel,
       true /* skip unk */,
-      true /* fallback2Letter */);
+      FLAGS_usewordpiece /* fallback2LetterWordSepLeft */,
+      !FLAGS_usewordpiece /* fallback2LetterWordSepLeft */);
 
   auto inputTransform = inputFeatures(
       featParams, featType, {FLAGS_localnrmlleftctx, FLAGS_localnrmlrightctx});

flashlight/app/asr/data/FeatureTransforms.cpp
@@ -94,14 +94,18 @@ fl::Dataset::DataTransformFunction targetFeatures(
         tokenDict,
         config.wordSeparator_,
         config.targetSamplePct_,
-        config.fallbackToLetter_,
+        config.fallbackToLetterWordSepLeft_,
+        config.fallbackToLetterWordSepRight_,
         config.skipUnk_);
     auto tgtVec = tokenDict.mapEntriesToIndices(target);
     if (!config.surround_.empty()) {
       // add surround token at the beginning and end of target
+      // only if begin/end tokens are not surround
       auto idx = tokenDict.getIndex(config.surround_);
-      tgtVec.emplace_back(idx);
-      if (tgtVec.size() > 1) {
+      if (tgtVec.empty() || tgtVec.back() != idx) {
+        tgtVec.emplace_back(idx);
+      }
+      if (tgtVec.size() > 1 && tgtVec.front() != idx) {
         tgtVec.emplace_back(idx);
         std::rotate(tgtVec.begin(), tgtVec.end() - 1, tgtVec.end());
       }

flashlight/app/asr/data/Utils.cpp
@@ -25,19 +25,32 @@ std::vector<std::string> wrd2Target(
     const Dictionary& dict,
     bool fallback2Ltr /* = false */,
     bool skipUnk /* = false */) {
+  bool fallback2LtrWordSepLeft = fallback2Ltr && FLAGS_usewordpiece;
+  bool fallback2LtrWordSepRight = fallback2Ltr && !FLAGS_usewordpiece;
   return wrd2Target(
-      word, lexicon, dict, FLAGS_sampletarget, fallback2Ltr, skipUnk);
+      word, 
+      lexicon, 
+      dict, 
+      FLAGS_wordseparator,
+      FLAGS_sampletarget, 
+      fallback2LtrWordSepLeft, 
+      fallback2LtrWordSepRight, 
+      skipUnk);
 }
 
 std::vector<std::string> wrd2Target(
     const std::string& word,
     const LexiconMap& lexicon,
     const Dictionary& dict,
+    const std::string& wordSeparator /* = "" */,
     float targetSamplePct /* = 0 */,
-    bool fallback2Ltr /* = false */,
+    bool fallback2LtrWordSepLeft /* = false */,
+    bool fallback2LtrWordSepRight /* = false */,
     bool skipUnk /* = false */) {
+  // find the word in the lexicon and use its spelling
   auto lit = lexicon.find(word);
   if (lit != lexicon.end()) {
+    // sample random spelling if word has different spellings
     if (lit->second.size() > 1 &&
         targetSamplePct >
             static_cast<float>(std::rand()) / static_cast<float>(RAND_MAX)) {
@@ -45,14 +58,18 @@ std::vector<std::string> wrd2Target(
     } else {
       return lit->second[0];
     }
-  }
-
-  std::vector<std::string> res;
-  if (fallback2Ltr) {
+  } 
+  
+  std::vector<std::string> word2tokens;
+  if (fallback2LtrWordSepLeft || fallback2LtrWordSepRight) {
+    if (fallback2LtrWordSepLeft && !wordSeparator.empty()) {
+      // add word separator at the beginning of fallback word 
+      word2tokens.push_back(wordSeparator);
+    } 
     auto tokens = splitWrd(word);
     for (const auto& tkn : tokens) {
       if (dict.contains(tkn)) {
-        res.push_back(tkn);
+        word2tokens.push_back(tkn);
       } else if (!skipUnk) {
         throw std::invalid_argument(
             "Unknown token '" + tkn +
@@ -60,10 +77,14 @@ std::vector<std::string> wrd2Target(
             word);
       }
     }
+    if (fallback2LtrWordSepRight && !wordSeparator.empty()) {
+      // add word separator at the end of fallback word 
+      word2tokens.push_back(wordSeparator);
+    }
   } else if (!skipUnk) {
     throw std::invalid_argument("Unknown word in the lexicon: " + word);
   }
-  return res;
+  return word2tokens;
 }
 
 std::vector<std::string> wrd2Target(
@@ -72,13 +93,16 @@ std::vector<std::string> wrd2Target(
     const Dictionary& dict,
     bool fallback2Ltr /* = false */,
     bool skipUnk /* = false */) {
+  bool fallback2LtrWordSepLeft = fallback2Ltr && FLAGS_usewordpiece;
+  bool fallback2LtrWordSepRight = fallback2Ltr && !FLAGS_usewordpiece;
   return wrd2Target(
       words,
       lexicon,
       dict,
       FLAGS_wordseparator,
       FLAGS_sampletarget,
-      fallback2Ltr,
+      fallback2LtrWordSepLeft,
+      fallback2LtrWordSepRight,
       skipUnk);
 }
 
@@ -88,38 +112,26 @@ std::vector<std::string> wrd2Target(
     const Dictionary& dict,
     const std::string& wordSeparator /* = "" */,
     float targetSamplePct /* = 0 */,
-    bool fallback2Ltr /* = false */,
+    bool fallback2LtrWordSepLeft /* = false */,
+    bool fallback2LtrWordSepRight /* = false */,
     bool skipUnk /* = false */) {
   std::vector<std::string> res;
   for (auto w : words) {
-    auto t =
-        wrd2Target(w, lexicon, dict, targetSamplePct, fallback2Ltr, skipUnk);
+    auto w2tokens =
+        wrd2Target(
+          w, 
+          lexicon, 
+          dict, 
+          wordSeparator,
+          targetSamplePct, 
+          fallback2LtrWordSepLeft, 
+          fallback2LtrWordSepRight, 
+          skipUnk);
 
-    if (t.size() == 0) {
+    if (w2tokens.size() == 0) {
       continue;
     }
-
-    // remove duplicate word separators in the beginning of each target token
-    if (res.size() > 0 && !wordSeparator.empty() &&
-        t[0].length() >= wordSeparator.length() &&
-        t[0].compare(0, wordSeparator.length(), wordSeparator) == 0) {
-      res.pop_back();
-    }
-
-    res.insert(res.end(), t.begin(), t.end());
-
-    if (!wordSeparator.empty() &&
-        !(res.back().length() >= wordSeparator.length() &&
-          res.back().compare(
-              res.back().length() - wordSeparator.length(),
-              wordSeparator.length(),
-              wordSeparator) == 0)) {
-      res.emplace_back(wordSeparator);
-    }
-  }
-
-  if (res.size() > 0 && res.back() == wordSeparator) {
-    res.pop_back();
+    res.insert(res.end(), w2tokens.begin(), w2tokens.end());
   }
   return res;
 }

flashlight/app/asr/data/Utils.h
@@ -35,8 +35,10 @@ std::vector<std::string> wrd2Target(
     const std::string& word,
     const lib::text::LexiconMap& lexicon,
     const lib::text::Dictionary& dict,
+    const std::string& wordSeparator = "",
     float targetSamplePct = 0,
-    bool fallback2Ltr = false,
+    bool fallback2LtrWordSepLeft = false,
+    bool fallback2LtrWordSepRight = false,
     bool skipUnk = false);
 
 std::vector<std::string> wrd2Target(
@@ -45,7 +47,8 @@ std::vector<std::string> wrd2Target(
     const lib::text::Dictionary& dict,
     const std::string& wordSeparator = "",
     float targetSamplePct = 0,
-    bool fallback2Ltr = false,
+    bool fallback2LtrWordSepLeft = false,
+    bool fallback2LtrWordSepRight = false,
     bool skipUnk = false);
 } // namespace asr
 } // namespace app

flashlight/app/asr/test/data/DataTest.cpp
@@ -12,6 +12,7 @@
 
 #include "flashlight/app/asr/common/Defines.h"
 #include "flashlight/app/asr/data/Featurize.h"
+#include "flashlight/app/asr/data/Utils.h"
 #include "flashlight/app/asr/data/ListFilesDataset.h"
 
 #include "flashlight/lib/common/String.h"
@@ -60,8 +61,8 @@ std::vector<std::string> loadTarget(const std::string& filepath) {
   }
   std::string line;
   while (std::getline(infile, line)) {
-    auto tkns_str = splitOnWhitespace(line, true);
-    for (const auto& tkn : tkns_str) {
+    auto tknsStr = splitOnWhitespace(line, true);
+    for (const auto& tkn : tknsStr) {
       tokens.emplace_back(tkn);
     }
   }

flashlight/app/asr/test/data/FeatureTest.cpp
@@ -130,6 +130,108 @@ TEST(FeatureTest, localNormalize) {
   }
 }
 
+TEST(FeatureTest, TargetTknTestStandaloneSep) {
+  Dictionary tokens;
+  std::string sep = "||";
+  tokens.addEntry("ab");
+  tokens.addEntry("cd");
+  tokens.addEntry("ef");
+  tokens.addEntry("t");
+  tokens.addEntry("r");
+  tokens.addEntry(sep);
+  
+  LexiconMap lexicon;
+  lexicon["abcd"].push_back({"ab", "cd", "||"});
+  lexicon["abcdef"].push_back({"ab", "cd", "ef", "||"});
+  
+  std::vector<std::string> words = {"abcdef", "abcd", "tr"};
+  auto res = wrd2Target(
+    words,
+    lexicon,
+    tokens,
+    sep,
+    0,
+    false,
+    true, // fallback right 
+    false
+  );
+
+  std::vector<std::string> resT = {"ab", "cd", "ef", "||", "ab", "cd", "||", "t", "r", "||"};
+  ASSERT_EQ(res.size(), resT.size());
+  for (int index = 0; index < res.size(); index++) {
+    ASSERT_EQ(res[index], resT[index]);
+  }
+
+  auto res2 = wrd2Target(
+    words,
+    lexicon,
+    tokens,
+    sep,
+    0,
+    true, // fallback left
+    false, 
+    false
+  );
+
+  std::vector<std::string> resT2 = {"ab", "cd", "ef", "||", "ab", "cd", "||", "||", "t", "r"};
+  ASSERT_EQ(res2.size(), resT2.size());
+  for (int index = 0; index < res2.size(); index++) {
+    ASSERT_EQ(res2[index], resT2[index]);
+  }
+}
+
+TEST(FeatureTest, TargetTknTestInsideSep) {
+  Dictionary tokens;
+  std::string sep = "_";
+  tokens.addEntry("_hel");
+  tokens.addEntry("lo");
+  tokens.addEntry("_ma");
+  tokens.addEntry("ma");
+  tokens.addEntry(sep);
+  tokens.addEntry("f");
+  tokens.addEntry("a");
+  
+  LexiconMap lexicon;
+  lexicon["hello"].push_back({"_hel", "lo"});
+  lexicon["mama"].push_back({"_ma", "ma"});
+  lexicon["af"].push_back({"_", "a", "f"});
+  
+  std::vector<std::string> words = {"aff", "hello", "mama", "af"};
+  auto res = wrd2Target(
+    words,
+    lexicon,
+    tokens,
+    sep,
+    0,
+    true, // fallback left
+    false,
+    false
+  );
+
+  std::vector<std::string> resT = {"_", "a", "f", "f", "_hel", "lo", "_ma", "ma", "_", "a", "f"};
+  ASSERT_EQ(res.size(), resT.size());
+  for (int index = 0; index < res.size(); index++) {
+    ASSERT_EQ(res[index], resT[index]);
+  }
+
+  auto res2 = wrd2Target(
+    words,
+    lexicon,
+    tokens,
+    sep,
+    0,
+    false,
+    true, // fallback right
+    false
+  );
+
+  std::vector<std::string> resT2 = {"a", "f", "f", "_", "_hel", "lo", "_ma", "ma", "_", "a", "f"};
+  ASSERT_EQ(res.size(), resT2.size());
+  for (int index = 0; index < res2.size(); index++) {
+    ASSERT_EQ(res2[index], resT2[index]);
+  }
+}
+
 TEST(FeatureTest, WrdToTarget) {
   gflags::FlagSaver flagsaver;
   FLAGS_wordseparator = "_";
@@ -145,8 +247,8 @@ TEST(FeatureTest, WrdToTarget) {
   lexicon["105"].push_back({"10", "5"});
   lexicon["2100"].push_back({"2", "1", "00"});
   // letters
-  lexicon["888"].push_back({"8", "8", "8"});
-  lexicon["12"].push_back({"1", "2"});
+  lexicon["888"].push_back({"8", "8", "8", "_"});
+  lexicon["12"].push_back({"1", "2", "_"});
   lexicon[kUnkToken] = {};
 
   Dictionary dict;
@@ -159,7 +261,9 @@ TEST(FeatureTest, WrdToTarget) {
       }
     }
   }
-  dict.addEntry("_");
+  if (!dict.contains(FLAGS_wordseparator)) {
+    dict.addEntry(FLAGS_wordseparator);
+  }
 
   std::vector<std::string> words = {"123", "456"};
   auto target = wrd2Target(words, lexicon, dict, false, false);
@@ -172,22 +276,30 @@ TEST(FeatureTest, WrdToTarget) {
   std::vector<std::string> words2 = {"105", "2100"};
   auto target2 = wrd2Target(words2, lexicon, dict, false, false);
   ASSERT_THAT(
-      target2, ::testing::ElementsAreArray({"10", "5", "_", "2", "1", "00"}));
+      target2, ::testing::ElementsAreArray({"10", "5", "2", "1", "00"}));
 
   std::vector<std::string> words3 = {"12", "888", "12"};
   auto target3 = wrd2Target(words3, lexicon, dict, false, false);
   ASSERT_THAT(
       target3,
       ::testing::ElementsAreArray(
-          {"1", "2", "_", "8", "8", "8", "_", "1", "2"}));
+          {"1", "2", "_", "8", "8", "8", "_", "1", "2", "_"}));
 
   // unknown words "111", "199"
   std::vector<std::string> words4 = {"111", "789", "199"};
-  // fall back to letters and skip unknown
+  // fall back to letters and skip unknown, word sep is part of word
+  FLAGS_usewordpiece = true;
   auto target4 = wrd2Target(words4, lexicon, dict, true, true);
   ASSERT_THAT(
-      target4,
-      ::testing::ElementsAreArray({"1", "1", "1", "_7", "89", "_", "1"}));
+    target4, 
+    ::testing::ElementsAreArray({"_", "1", "1", "1", "_7", "89", "_", "1"}));
+  // fall back to letters and skip unknown, word sep is separate token
+  FLAGS_usewordpiece = false;
+  target4 = wrd2Target(words4, lexicon, dict, true, true);
+  ASSERT_THAT(
+    target4, 
+    ::testing::ElementsAreArray({"1", "1", "1", "_", "_7", "89", "1", "_"}));
+
   // skip unknown
   target4 = wrd2Target(words4, lexicon, dict, false, true);
   ASSERT_THAT(target4, ::testing::ElementsAreArray({"_7", "89"}));

commit e79ecc9b2c451c9d5a595e9a71182f9336092dad

fix bug introduced in the https://github.com/facebookresearch/flashlight/commit/e83e7b6720f9407bc706d6c26f0a599dc0941285#diff-952e23cfc83bb7703265b4c2f0c62de5e32a4d27d4f67c037c6873aeec3989f0L33

flashlight/app/asr/data/FeatureTransforms.h
@@ -31,7 +31,8 @@ struct TargetGenerationConfig {
       bool skipUnk,
       bool fallbackToLetterWordSepLeft,
       bool fallbackToLetterWordSepRight)
-      : targetSamplePct_(targetSamplePct),
+      : wordSeparator_(wordSeparator),
+	targetSamplePct_(targetSamplePct),
         criterion_(criterion),
         surround_(surround),
         eosToken_(eosToken),