2ced9a43fa5ce7f3be39aeddcc81bb5f1fb8c2f2
Jacob Kahn <jacobkahn@fb.com>
2020-11-19 08:19:51 -0800

    Fix stb image search, link gflags, conditionally build imagenet34 example
    Differential Revision: D25091576
    
    fbshipit-source-id: 33ddac6706a2563302eb0ef7a9e59992480cbfa5

--- cmake/FindGFLAGS.cmake
+++ cmake/FindGFLAGS.cmake
@@ -18,7 +18,7 @@ find_path(GFLAGS_INCLUDE_DIR gflags/gflags.h
 
 find_library(GFLAGS_LIBRARY NAMES gflags gflags_debug)
 
-find_package_handle_standard_args(GFlags DEFAULT_MSG GFLAGS_INCLUDE_DIR GFLAGS_LIBRARY)
+find_package_handle_standard_args(GFLAGS DEFAULT_MSG GFLAGS_INCLUDE_DIR GFLAGS_LIBRARY)
 
 if(GFLAGS_FOUND)
   set(GFLAGS_INCLUDE_DIRS ${GFLAGS_INCLUDE_DIR})
