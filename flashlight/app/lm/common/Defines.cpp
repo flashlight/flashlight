5c87997023d30093089f09eee8a25624a3622f9a
Qiantong Xu <qiantong@fb.com>
2020-12-02 15:37:44 -0800

    Opening folder app/lm, together with dictionary builder
    Differential Revision: D25109760
    
    fbshipit-source-id: 47372e1d23a89ebabe86b5dc8aa86b2e0361e647

---
+++ flashlight/app/lm/common/Defines.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/app/lm/common/Defines.h"
+
+namespace fl {
+namespace app {
+namespace lm {
+
+/* Place holder */
+
+} // namespace lm
+} // namespace app
+} // namespace fl
