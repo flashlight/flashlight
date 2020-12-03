ccf334597a2427e32dcdf5a1088c4c4e8e5faf37
Gilad Avidov <avidov@fb.com>
2020-12-03 14:41:59 -0800

    sound effect augmentation library (#182)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/182
    
    Add sound effect library for high performance speech augmentation.
    
    Currently implemented effects:
    
    - fixed/random amplification
    - normalization
    - clamping
    
    Reviewed By: vineelpratap, tlikhomanenko
    
    Differential Revision: D23921685
    
    fbshipit-source-id: 077337eb04d466ac34eab1a3d1081fc647e6b2d6

---
+++ flashlight/app/asraug/test/SoundTestUtil.cpp
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/app/asraug/test/SoundTestUtil.h"
+
+#include <cmath>
+
+namespace fl {
+namespace app {
+namespace sfx {
+
+std::vector<float>
+genSinWave(size_t numSamples, size_t freq, size_t sampleRate, float amplitude) {
+  std::vector<float> output(numSamples, 0);
+  const float waveLenSamples =
+      static_cast<float>(sampleRate) / static_cast<float>(freq);
+  const float ratio = (2 * M_PI) / waveLenSamples;
+
+  for (size_t i = 0; i < numSamples; ++i) {
+    output.at(i) = amplitude * std::sin(static_cast<float>(i) * ratio);
+  }
+  return output;
+}
+
+
+} // namespace sfx
+} // namespace app
+} // namespace fl
---
+++ flashlight/app/asraug/test/SoundTestUtil.h
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <vector>
+
+namespace fl {
+namespace app {
+namespace sfx {
+
+std::vector<float>
+genSinWave(size_t numSamples, size_t freq, size_t sampleRate, float amplitude);
+
+} // namespace sfx
+} // namespace app
+} // namespace fl
