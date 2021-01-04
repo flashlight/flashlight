fcca27ced73c36db933f768e08122d53ad4dc914
Jacob Kahn <jacobkahn@fb.com>
2021-01-04 09:03:03 -0800

    Move syncMeters out of ext Utils, reorg dist utils (#388)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/388
    
    This didn't compile sometimes - clean it up
    
    Reviewed By: tlikhomanenko
    
    Differential Revision: D25736159
    
    fbshipit-source-id: 9d992788703f81a339ce853c00d4ad57814259d4

--- flashlight/ext/common/DistributedUtils.h
+++ flashlight/ext/common/DistributedUtils.h
@@ -16,24 +16,6 @@ namespace fl {
 namespace ext {
 
 /**
- * Convert an arrayfire array into a std::vector.
- *
- * @param arr input array to convert
- *
- */
-template <typename T>
-std::vector<T> afToVector(const af::array& arr);
-
-/**
- * Convert the array in a Variable into a std::vector.
- *
- * @param var input Variables to convert
- *
- */
-template <typename T>
-std::vector<T> afToVector(const fl::Variable& var);
-
-/**
  * Call Flashlight API to initialize distributed environment.
  */
 void initDistributed(
@@ -41,18 +23,6 @@ void initDistributed(
     int worldSize,
     int maxDevicesPerNode,
     const std::string& rndvFilepath);
-
-/**
- * Synchronize meters across process.
- */
-template <typename T>
-void syncMeter(T& mtr);
-
-template void syncMeter<AverageValueMeter>(AverageValueMeter& mtr);
-template void syncMeter<EditDistanceMeter>(EditDistanceMeter& mtr);
-template void syncMeter<CountMeter>(CountMeter& mtr);
-template void syncMeter<TimeMeter>(TimeMeter& mtr);
-template void syncMeter<TopKMeter>(TopKMeter& mtr);
 
 af::array allreduceGet(AverageValueMeter& mtr);
 af::array allreduceGet(EditDistanceMeter& mtr);
@@ -65,6 +35,25 @@ void allreduceSet(EditDistanceMeter& mtr, af::array& val);
 void allreduceSet(CountMeter& mtr, af::array& val);
 void allreduceSet(TimeMeter& mtr, af::array& val);
 void allreduceSet(TopKMeter& mtr, af::array& val);
+
+/**
+ * Synchronize meters across process.
+ */
+template <typename T>
+void syncMeter(T& mtr) {
+  if (!fl::isDistributedInit()) {
+    return;
+  }
+  af::array arr = allreduceGet(mtr);
+  fl::allReduce(arr);
+  allreduceSet(mtr, arr);
+}
+
+template void syncMeter<AverageValueMeter>(AverageValueMeter& mtr);
+template void syncMeter<EditDistanceMeter>(EditDistanceMeter& mtr);
+template void syncMeter<CountMeter>(CountMeter& mtr);
+template void syncMeter<TimeMeter>(TimeMeter& mtr);
+template void syncMeter<TopKMeter>(TopKMeter& mtr);
 
 } // namespace ext
 } // namespace fl
