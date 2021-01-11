b7a23135f8433e7fcecd575356be3ed167827c25
Tatiana Likhomanenko <antares@fb.com>
2021-01-11 15:51:53 -0800

    fix attention for encoder-decoder by adding support of input sizes of encoder (#327)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/327
    
    Support padding mask for encoder output = decoder input for the attention in encoder-decoder part.
    - add extra argument for attention forward, which specifies not padded sizes of encoder output
    - if mask is provided set -inf for log attention weights on these position, so after softmax there will be zero at padded positions. Thus decoder will never attend to the padded part of the encoder output
    - Assume that extra attention weights provided in forward now in log scale to avoid extra log(exp) computations in the pre-training window. (Pre-training window will be fixed in the next diff to return log scaled attention)
    - For now this fix doesn't change s2s models criteria, use the old behaviour for now.
    
    Reviewed By: xuqiantong
    
    Differential Revision: D25415790
    
    fbshipit-source-id: 9e3ddb1b4e61daa1a7d574c56d2277baf2efb6f9

--- flashlight/app/asr/criterion/attention/AttentionBase.h
+++ flashlight/app/asr/criterion/attention/AttentionBase.h
@@ -13,48 +13,80 @@ namespace fl {
 namespace app {
 namespace asr {
 
+/**
+ * Attention base class for encoder-decoder: decoder attends to particular
+ * encoder part
+ */
 class AttentionBase : public fl::Container {
  public:
   AttentionBase() {}
 
-  std::vector<fl::Variable> forward(
-      const std::vector<fl::Variable>& inputs) override {
-    if (inputs.size() != 3 && inputs.size() != 4) {
-      throw std::invalid_argument("Invalid inputs size");
+  std::vector<Variable> forward(const std::vector<Variable>& inputs) override {
+    if (inputs.size() != 3 && inputs.size() != 4 && inputs.size() != 5) {
+      throw std::invalid_argument(
+          "Attention encoder-decoder: Invalid inputs size, should be 3, 4, or 5 arguments");
     }
 
-    auto attnWeight = inputs.size() == 4 ? inputs[3] : fl::Variable();
-    auto res = forward(inputs[0], inputs[1], inputs[2], attnWeight);
+    auto logAttnWeight = inputs.size() == 4 ? inputs[3] : Variable();
+    auto xEncodedSizes = inputs.size() == 5 ? inputs[4] : Variable();
+    auto res = forwardBase(
+        inputs[0], inputs[1], inputs[2], logAttnWeight, xEncodedSizes);
     return {res.first, res.second};
   }
 
-  std::pair<fl::Variable, fl::Variable> operator()(
-      const fl::Variable& state,
-      const fl::Variable& xEncoded,
-      const fl::Variable& prevAttn) {
-    return forward(state, xEncoded, prevAttn);
+  std::pair<Variable, Variable> forward(
+      const Variable& state,
+      const Variable& xEncoded,
+      const Variable& prevAttn) {
+    return forward(
+        state,
+        xEncoded,
+        prevAttn,
+        Variable() /* logAttnWeight */,
+        Variable() /* xEncodedSizes */);
   }
 
-  std::pair<fl::Variable, fl::Variable> forward(
-      const fl::Variable& state,
-      const fl::Variable& xEncoded,
-      const fl::Variable& prevAttn) {
-    return forward(state, xEncoded, prevAttn, fl::Variable() /* attnWeight */);
+  std::pair<Variable, Variable> forward(
+      const Variable& state,
+      const Variable& xEncoded,
+      const Variable& prevAttn,
+      const Variable& logAttnWeight) {
+    return forwardBase(
+        state,
+        xEncoded,
+        prevAttn,
+        logAttnWeight,
+        Variable() /* xEncodedSizes */);
   }
 
-  std::pair<fl::Variable, fl::Variable> operator()(
-      const fl::Variable& state,
-      const fl::Variable& xEncoded,
-      const fl::Variable& prevAttn,
-      const fl::Variable& attnWeight) {
-    return forward(state, xEncoded, prevAttn, attnWeight);
+  virtual std::pair<Variable, Variable> forward(
+      const Variable& state,
+      const Variable& xEncoded,
+      const Variable& prevAttn,
+      const Variable& logAttnWeight,
+      const Variable& xEncodedSizes) {
+    return forwardBase(state, xEncoded, prevAttn, logAttnWeight, xEncodedSizes);
   }
 
-  virtual std::pair<fl::Variable, fl::Variable> forward(
-      const fl::Variable& state,
-      const fl::Variable& xEncoded,
-      const fl::Variable& prevAttn,
-      const fl::Variable& attnWeight) = 0;
+ protected:
+  /**
+   * Forward pass
+   * @param state current decoder state
+   * @param xEncoded encoder output = decoder input
+   * @param prevAttn previous attention vector
+   * @param logAttnWeight attention weights to add: finalAttn =
+   * exp(logAttnWeight + logComputedAttn)
+   * @param xEncodedSizes encoder output actual sizes has (1, B) dims
+   * Returns <attention vector (sum = 1), summary> of sizes
+   * [targetlen, seqlen, batchsize] for attention,
+   * [hiddendim, targetlen, batchsize] for summary
+   */
+  virtual std::pair<Variable, Variable> forwardBase(
+      const Variable& state,
+      const Variable& xEncoded,
+      const Variable& prevAttn,
+      const Variable& logAttnWeight,
+      const Variable& xEncodedSizes) = 0;
 
  private:
   FL_SAVE_LOAD_WITH_BASE(fl::Container)
