08ffac909a5c7baca93314487ec575a305330e9f
Your Name <you@example.com>
2021-01-29 11:38:50 -0800

    add mkl library wrapper with impemntation of conv1D (#443)
    Summary:
    MKL Vector Statistical Library (VSL) fetures convolution and cross correlation routines as well as
    commonly used pseudo- or quasi-random number generators with continuous and discrete distribution.
    This diff adds a wrapper for conv1D which performed x3000 faster then hand written nested loops
    on sound effect RIR application.
    
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/443
    
    Differential Revision: D26132123
    
    Pulled By: avidov
    
    fbshipit-source-id: 1d9ca2a2f8f0265425cb8caab9913b914454d12b

---
+++ flashlight/lib/test/mkl/ConvTest.cpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "flashlight/fl/common/Init.h"
+#include "flashlight/lib/mkl/Conv.h"
+
+#include <iostream>
+
+using namespace ::fl::lib;
+using ::testing::ElementsAre;
+using ::testing::Pointwise;
+
+MATCHER_P(FloatNearPointwise, tol, "Out of range") {
+  return (
+      std::get<0>(arg) > std::get<1>(arg) - tol &&
+      std::get<0>(arg) < std::get<1>(arg) + tol);
+}
+
+TEST(Conv1DTest, Identity) {
+  std::vector<float> kernel = {1};
+  std::vector<float> input = {1, 2, 3, 4, 5};
+  std::vector<float> output = fl::lib::mkl::conv1D(kernel, input);
+  EXPECT_EQ(output.size(), input.size() + kernel.size() - 1);
+  EXPECT_THAT(output, Pointwise(FloatNearPointwise(0.01), input));
+}
+
+TEST(Conv1DTest, BasicReverb) {
+  std::vector<float> kernel = {1, 2, 3};
+  std::vector<float> input = {1, 2, 3, 4, 5};
+  std::vector<float> output = fl::lib::mkl::conv1D(kernel, input);
+  EXPECT_EQ(output.size(), input.size() + kernel.size() - 1);
+  EXPECT_THAT(output, ElementsAre(1, 4, 10, 16, 22, 22, 15));
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
