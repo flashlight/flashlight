8b2087d59cd13f160500ec641b2746797cab227a
Gilad Avidov <avidov@fb.com>
2021-03-15 13:03:50 -0700

    disable caching memory manager for opencl
    Summary:
    Custom opencl kernel crash when using the caching memory manager.
    This is a temporary workaround.
    
    Differential Revision: D26831345
    
    fbshipit-source-id: 99f464a9a3c9356c637b5738395644b2c1620fa9

--- flashlight/fl/common/Init.cpp
+++ flashlight/fl/common/Init.cpp
@@ -29,7 +29,9 @@ void init() {
     // TODO: remove this temporary workaround for TextDatasetTest crash on CPU
     // backend when tearing down the test environment. This is possibly due to
     // AF race conditions when tearing down our custom memory manager.
-    if (!FL_BACKEND_CPU) {
+    // TODO: remove this temporary workaround for crashes when using custom
+    // opencl kernels.
+    if (FL_BACKEND_CUDA) {
       MemoryManagerInstaller::installDefaultMemoryManager();
     }
   });
