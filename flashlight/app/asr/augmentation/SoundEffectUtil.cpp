e95bf578d99e2b34f56e538b0f7502bd910085d0
Your Name <you@example.com>
2021-03-29 17:32:22 -0700

    time stretch (#394)
    Summary:
    Add time stretch using sox's algorithm but without libsox threading issues.
    The algorithm is almost verbatim copied to make review simpler.
    Changes to original code applied only where required.
    
    Pull Request resolved: https://github.com/facebookresearch/flashlight/pull/394
    
    Test Plan:
    20% faster
    factor 1.2
    {F398927521}
    
    Original
    {F398929830}
    
    20% s
    lower
    Factor 0.8
    {F398931535}
    
    Reviewed By: vineelpratap
    
    Differential Revision: D25780042
    
    Pulled By: avidov
    
    fbshipit-source-id: 887c69d2f465b51e536b3f9f1fe8653c6e14e048

--- flashlight/app/asr/augmentation/SoundEffectUtil.cpp
+++ flashlight/app/asr/augmentation/SoundEffectUtil.cpp
@@ -54,6 +54,19 @@ float signalToNoiseRatio(
   return 20 * std::log10(singalRms / noiseRms);
 }
 
+std::vector<float>
+genTestSinWave(size_t numSamples, size_t freq, size_t sampleRate, float amplitude) {
+  std::vector<float> output(numSamples, 0);
+  const float waveLenSamples =
+      static_cast<float>(sampleRate) / static_cast<float>(freq);
+  const float ratio = (2 * M_PI) / waveLenSamples;
+
+  for (size_t i = 0; i < numSamples; ++i) {
+    output.at(i) = amplitude * std::sin(static_cast<float>(i) * ratio);
+  }
+  return output;
+}
+
 } // namespace sfx
 } // namespace asr
 } // namespace app
