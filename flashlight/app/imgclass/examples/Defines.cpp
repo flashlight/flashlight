0f1942de27b197458fa7cdec9c39c666eeec9672
Qiantong Xu <qiantong@fb.com>
2021-04-01 12:19:31 -0700

    ViT train.cpp
    Differential Revision: D27376373
    
    fbshipit-source-id: 2d4705e065ee443cd4023251872f524879e9d1ab

---
+++ flashlight/app/imgclass/examples/Defines.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/app/imgclass/examples/Defines.h"
+
+namespace fl {
+namespace app {
+namespace image {
+
+const std::vector<float> kImageNetMean = {0.485, 0.456, 0.406};
+const std::vector<float> kImageNetStd = {0.229, 0.224, 0.225};
+
+} // namespace image
+} // namespace app
+} // namespace fl
