a57671399bbe76c1ae8ef1949a615b7472514d6e
Vineel Pratap <vineelkpratap@fb.com>
2021-04-06 15:13:31 -0700

    Simplify clip grad norm API (minor speedup)
    Summary:
    - Reduce number of AF functions called
    - Add 1e-6 to denominator to avoid division by zero
    
    Created from Diffusion's 'Open in Editor' feature.
    
    Reviewed By: jacobkahn, tlikhomanenko
    
    Differential Revision: D27243936
    
    fbshipit-source-id: e993ed7cdb2fa36cf31c03e38778a7bfa507f7e4

--- flashlight/fl/optim/Utils.cpp
+++ flashlight/fl/optim/Utils.cpp
@@ -9,35 +9,27 @@
 
 namespace fl {
 
-double clipGradNorm(const std::vector<Variable>& parameters, double max_norm) {
-  double grad_norm = 0.0;
+double clipGradNorm(const std::vector<Variable>& parameters, double maxNorm) {
+  double gradNorm = 0.0;
   for (const auto& p : parameters) {
     if (!p.isGradAvailable()) {
       continue;
     }
     const auto& grad = p.grad().array();
-    // ArrayFire v3.7.1 doesn't support computing the norm of an f16 tensor.
-    // Only if gradients are fp16, compute the norm on grads that are cast to
-    // f32. This cast can be removed when support for fp16 inputs is added.
-    // https://github.com/arrayfire/arrayfire/blob/v3.7.1/src/api/c/norm.cpp#L128
-    grad_norm += std::pow(
-        af::norm(af::flat(
-            (grad.type() == af::dtype::f16 ? grad.as(af::dtype::f32) : grad))),
-        2);
+    gradNorm += af::sum<double>(grad * grad);
   }
-  grad_norm = std::sqrt(grad_norm);
-  double scale = (max_norm / grad_norm);
-  if (scale < 1.0) {
-    for (auto& p : parameters) {
-      if (!p.isGradAvailable()) {
-        continue;
-      }
-      auto& grad = p.grad().array();
-      grad = scale * grad;
-      p.grad().array() = grad;
+  gradNorm = std::sqrt(gradNorm);
+  double scale = maxNorm / (gradNorm + 1e-6);
+  if (scale >= 1.0) {
+    return gradNorm;
+  }
+  for (auto& p : parameters) {
+    if (!p.isGradAvailable()) {
+      continue;
     }
+    p.grad().array() *= scale;
   }
-  return grad_norm;
+  return gradNorm;
 }
 
 } // namespace fl
