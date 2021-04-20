8ac144c2141e2d898490405f058bdbfa17e1494f
jacobkahn <jacobkahn1@gmail.com>
2021-04-20 16:49:47 -0700

    Typos, remove gtest and af dirs from cov

--- cmake/InternalUtils.cmake
+++ cmake/InternalUtils.cmake
@@ -17,7 +17,7 @@ function(add_coverage_to_target)
     else()
       target_link_libraries(${add_coverage_to_target_TARGET}
         PUBLIC
-        --coverage>)
+        --coverage)
     endif()
   endif()
 endfunction()
