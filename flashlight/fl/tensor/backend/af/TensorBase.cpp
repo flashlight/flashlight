3d70494023abb7b7bb75c97abbe52982e436867c
Jacob Kahn <jacobkahn@fb.com>
2021-05-10 19:08:21 -0700

    Binary operator stubs
    Differential Revision: D28070407
    
    fbshipit-source-id: a0d440d22f576925d8e73daec44c7c5d414abb6b

--- flashlight/fl/tensor/backend/af/TensorBase.cpp
+++ flashlight/fl/tensor/backend/af/TensorBase.cpp
@@ -9,10 +9,71 @@
 
 namespace fl {
 
+/*
+ * Below this point are ArrayFire-specific implementations. They should be
+ * [re]moved to a specific ArrayFire backend implementation (along with other
+ * flashlight/fl/tensor assets once migration is complete).
+ */
+
 Tensor::Tensor(af::array&& array) : array_(std::move(array)) {}
 
 af::array& Tensor::getArray() {
   return array_;
 }
+
+const af::array& Tensor::getArray() const {
+  return array_;
+}
+
+/************************** Binary Operators ***************************/
+// For ArrayFire, af::array already implements overloads for all needed
+// operators -- use these by default.
+#define AF_BINARY_OP_TYPE_DEF(OP, TYPE)           \
+  Tensor operator OP(const Tensor& a, TYPE rhs) { \
+    return Tensor(a.getArray() OP rhs);           \
+  }                                               \
+  Tensor operator OP(TYPE lhs, const Tensor& a) { \
+    return Tensor(lhs OP a.getArray());           \
+  }
+#define AF_BINARY_OP_LITERALS_DEF(OP)                   \
+  AF_BINARY_OP_TYPE_DEF(OP, const bool&);               \
+  AF_BINARY_OP_TYPE_DEF(OP, const int&);                \
+  AF_BINARY_OP_TYPE_DEF(OP, const unsigned&);           \
+  AF_BINARY_OP_TYPE_DEF(OP, const char&);               \
+  AF_BINARY_OP_TYPE_DEF(OP, const unsigned char&);      \
+  AF_BINARY_OP_TYPE_DEF(OP, const long&);               \
+  AF_BINARY_OP_TYPE_DEF(OP, const unsigned long&);      \
+  AF_BINARY_OP_TYPE_DEF(OP, const long long&);          \
+  AF_BINARY_OP_TYPE_DEF(OP, const unsigned long long&); \
+  AF_BINARY_OP_TYPE_DEF(OP, const double&);             \
+  AF_BINARY_OP_TYPE_DEF(OP, const float&);              \
+  AF_BINARY_OP_TYPE_DEF(OP, const short&);              \
+  AF_BINARY_OP_TYPE_DEF(OP, const unsigned short&);
+// Operations on fl::Tensor call the respective operator overloads that are
+// already defined on af::arrays
+#define AF_BINARY_OP_DEF(OP, FUNC)                    \
+  Tensor FUNC(const Tensor& lhs, const Tensor& rhs) { \
+    return Tensor(lhs.getArray() OP rhs.getArray());  \
+  }                                                   \
+  AF_BINARY_OP_LITERALS_DEF(OP);
+// Definitions
+// Since ArrayFire implements operator overloads, map both fl::Tensor functions
+// and fl::Tensor operator overloads back to the af::array overloads.
+AF_BINARY_OP_DEF(+, add);
+AF_BINARY_OP_DEF(-, sub);
+AF_BINARY_OP_DEF(*, mul);
+AF_BINARY_OP_DEF(/, div);
+AF_BINARY_OP_DEF(==, eq);
+AF_BINARY_OP_DEF(!=, neq);
+AF_BINARY_OP_DEF(<, lessThan);
+AF_BINARY_OP_DEF(<=, lessThanEqual);
+AF_BINARY_OP_DEF(>, greaterThan);
+AF_BINARY_OP_DEF(>=, greaterThanEqual);
+AF_BINARY_OP_DEF(||, logicalOr);
+AF_BINARY_OP_DEF(%, mod);
+AF_BINARY_OP_DEF(|, bitwiseOr);
+AF_BINARY_OP_DEF(^, bitwiseXor);
+AF_BINARY_OP_DEF(<<, lShift);
+AF_BINARY_OP_DEF(>>, rShift);
 
 } // namespace fl
