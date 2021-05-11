e1900491701a7a23600eccce520fba698f94fb9a
Jacob Kahn <jacobkahn@fb.com>
2021-05-10 19:08:35 -0700

    Introduce fl::Shape, fl::dtype, and fl::full (#579)
    Summary:
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/579
    
    Introduce `fl::Shape` and `fl::dtype`. These represent tensor dimensions and tensor datatypes.
    
    For now, the default implementation wraps/converts to and from ArrayFire implementations for dimensions and shapes.
    
    Add `fl::full` (mirroring numpy -- https://numpy.org/doc/stable/reference/generated/numpy.full.html). `fl::full` does some implicit casting around input operators.
    
    These operators and functions continue to be defined in terms of ArrayFire most explicitly to make transitioning existing Flashlight code to `fl::Tensor` easier.
    
    See D28163616 for a draft of a generic shape implementation (`GenericShape` that doesn't depend on ArrayFire). Will be keeping these standalone implementations as draft commits and eventually adding more components to them as time goes on to support custom `fl::Tensor` implementations.
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D28138002
    
    fbshipit-source-id: cfea6db50f6c8a5db2ba1de66fee4c9cdf45b1d8

---
+++ flashlight/fl/tensor/ShapeBase.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <ostream>
+#include <utility>
+#include <vector>
+
+// TODO:fl::Tensor {af} remove me when ArrayFire is a particular subimpl
+#include <af/dim4.hpp>
+
+namespace fl {
+
+// The type of a dimension.
+using Dim = long long;
+
+/**
+ * A base interface for defining some notion of shape on top of a tensor. Can be
+ * constructed from an initializer list, inspected, and modified.
+ *
+ * TODO: for now, Shape has a af::dim4 member that hides core logic given that
+ * the default tensor lib implementation is ArrayFire. This will be removed once
+ * the transition to fl::Tensor across Flashlight is complete.
+ *
+ * TODO: make these methods pure virtual when migration is complete.
+ */
+struct Shape {
+  virtual ~Shape() = default;
+  Shape() = default;
+
+  // TODO:fl::Tensor {af} remove these when ArrayFire is a particular subimpl
+  af::dim4 dims_;
+  /**
+   * Gives the maximum number of dimensions a tensor of a particular shape can
+   * have.
+   *
+   * If the maximum size can be arbitrarily high, `std::numeric_limits<Dim>`
+   * should be used.
+   *
+   * TODO: remove me once migration is complete.
+   */
+  static constexpr size_t kMaxDims{AF_MAX_DIMS};
+
+  /**
+   * Initialize a Shape via a vector.
+   */
+  explicit Shape(std::vector<Dim> d);
+
+  /**
+   * Initialize a Shape via an initializer list.
+   */
+  /* implicit */ Shape(std::initializer_list<Dim> d);
+
+  /**
+   * @return the number of elements in a tensor that has the given shape.
+   */
+  virtual size_t elements() const;
+
+  /**
+   * @return Number of dimensions in the shape.
+   */
+  virtual size_t nDims() const;
+
+  /**
+   * Get the size of a given dimension. Throws if the given dimension is larger
+   * than the number of dimensions.
+   *
+   * @return the Dim at the given dimension
+   */
+  virtual Dim dim(size_t dim) const;
+
+  /**
+   * Compares two shapes. Returns true if their dim vectors are equal.
+   */
+  virtual bool operator==(const Shape& other) const;
+  virtual bool operator!=(const Shape& other) const;
+};
+
+std::ostream& operator<<(std::ostream& ostr, const Shape& s);
+
+} // namespace fl
---
+++ flashlight/fl/tensor/Types.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/fl/tensor/Types.h"
+
+#include <unordered_map>
+
+namespace fl {
+
+const std::unordered_map<dtype, std::string> kTypeToString = {
+    {dtype::f16, "f16"},
+    {dtype::f32, "f32"},
+    {dtype::f64, "f64"},
+    {dtype::b8, "b8"},
+    {dtype::s16, "s16"},
+    {dtype::s32, "s32"},
+    {dtype::s64, "s64"},
+    {dtype::u8, "u8"},
+    {dtype::u16, "u16"},
+    {dtype::u32, "u32"},
+    {dtype::u64, "u64"},
+};
+
+const std::string& dtypeToString(dtype type) {
+  return kTypeToString.at(type);
+}
+
+} // namespace fl
