3db5587b1a9c8895ae28907a5d551bc1c09d5753
Jacob Kahn <jacobkahn@fb.com>
2021-07-20 11:17:39 -0700

    Move tensor compute/rand functions to TensorBackend interface
    Summary: See title.
    
    Differential Revision: D29753101
    
    fbshipit-source-id: 81822adef1620bd693c5e744b8163f061d7530cb

---
+++ flashlight/fl/tensor/Compute.cpp
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/fl/tensor/Compute.h"
+
+#include <utility>
+
+#include "flashlight/fl/tensor/TensorBackend.h"
+#include "flashlight/fl/tensor/TensorBase.h"
+
+// TODO: remove me once no more `af::eval` calls
+#include <af/array.h>
+#include <af/device.h>
+#include "flashlight/fl/tensor/backend/af/ArrayFireTensor.h"
+
+namespace fl {
+
+void sync() {
+  Tensor().backend().sync();
+}
+
+void sync(int deviceId) {
+  Tensor().backend().sync(deviceId);
+}
+
+void eval(Tensor& tensor) {
+  Tensor().backend().eval(tensor);
+}
+
+// TODO:fl::Tensor remove once no more `af::eval` calls
+void eval(af::array& array) {
+  af::array a = array;
+  Tensor t = toTensor<ArrayFireTensor>(std::move(a));
+  fl::eval(t);
+}
+
+int getDevice() {
+  return Tensor().backend().getDevice();
+}
+
+void setDevice(int deviceId) {
+  Tensor().backend().setDevice(deviceId);
+}
+
+} // namespace fl
