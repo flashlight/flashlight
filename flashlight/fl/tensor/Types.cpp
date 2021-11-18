2830b9a371dc720a6ef374960879211d62ff7ce5
Jacob Kahn <jacobkahn@fb.com>
2021-11-18 12:00:33 -0800

    Better matmul shape consistency tests for bmm
    Summary: See title -- batch matmul semantics mirror TF's (and PT `torch.bmm`)
    
    Differential Revision: D31984655
    
    fbshipit-source-id: cdab78b46e256ae888ec095d00251dbf3cd7b3c5

--- flashlight/fl/tensor/Types.cpp
+++ flashlight/fl/tensor/Types.cpp
@@ -26,6 +26,20 @@ const std::unordered_map<dtype, std::string> kTypeToString = {
     {dtype::u64, "u64"},
 };
 
+const std::unordered_map<std::string, dtype> kStringToType = {
+    {"f16", dtype::f16},
+    {"f32", dtype::f32},
+    {"f64", dtype::f64},
+    {"b8", dtype::b8},
+    {"s16", dtype::s16},
+    {"s32", dtype::s32},
+    {"s64", dtype::s64},
+    {"u8", dtype::u8},
+    {"u16", dtype::u16},
+    {"u32", dtype::u32},
+    {"u64", dtype::u64},
+};
+
 size_t getTypeSize(dtype type) {
   switch (type) {
     case dtype::f16:
@@ -57,6 +71,13 @@ size_t getTypeSize(dtype type) {
 
 const std::string& dtypeToString(dtype type) {
   return kTypeToString.at(type);
+}
+
+fl::dtype stringToDtype(const std::string& string) {
+  if (kStringToType.find(string) != kStringToType.end()) {
+    return kStringToType.at(string);
+  }
+  throw std::invalid_argument("stringToDtype: Invalid input type: " + string);
 }
 
 std::ostream& operator<<(std::ostream& ostr, const dtype& s) {
