b3d0645b585c43226a0ce8d397c18e3126fb49f7
Jacob Kahn <jacobkahn@fb.com>
2021-11-30 09:28:07 -0800

    Add Vision TensorExtension
    fbshipit-source-id: ac27a015b09fb5fff87f66c4f622a22e365f359f

---
+++ flashlight/pkg/vision/tensor/VisionOps.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/pkg/vision/tensor/VisionOps.h"
+
+#include "flashlight/fl/tensor/TensorBackend.h"
+#include "flashlight/pkg/vision/tensor/VisionExtension.h"
+#include "flashlight/pkg/vision/tensor/VisionExtensionBackends.h"
+
+namespace fl {
+
+Tensor histogram(
+    const Tensor& tensor,
+    const unsigned numBins,
+    const double minVal,
+    const double maxVal) {
+  return tensor.backend().getExtension<VisionExtension>().histogram(
+      tensor, numBins, minVal, maxVal);
+}
+
+Tensor histogram(const Tensor& tensor, const unsigned numBins) {
+  return tensor.backend().getExtension<VisionExtension>().histogram(
+      tensor, numBins);
+}
+
+Tensor equalize(const Tensor& input, const Tensor& histogram) {
+  return input.backend().getExtension<VisionExtension>().equalize(
+      input, histogram);
+}
+
+Tensor resize(
+    const Tensor& tensor,
+    const Shape& shape,
+    const InterpolationMode mode /* = InterpolationMode::Nearest */) {
+  return tensor.backend().getExtension<VisionExtension>().resize(
+      tensor, shape, mode);
+}
+
+Tensor rotate(
+    const Tensor& input,
+    const float theta,
+    const Tensor& fill /* = Tensor() */) {
+  return input.backend().getExtension<VisionExtension>().rotate(
+      input, theta, fill);
+}
+
+Tensor rotate(
+    const Tensor& input,
+    const float theta,
+    const InterpolationMode mode /* = InterpolationMode::Nearest */) {
+  return input.backend().getExtension<VisionExtension>().rotate(
+      input, theta, mode);
+}
+
+Tensor translate(
+    const Tensor& input,
+    const Shape& translation,
+    const Shape& outputDims /* = {} */,
+    const Tensor& fill /* = Tensor() */) {
+  return input.backend().getExtension<VisionExtension>().translate(
+      input, translation, outputDims, fill);
+}
+
+Tensor translate(
+    const Tensor& input,
+    const Shape& translation,
+    const Shape& outputDims /* = {} */,
+    const InterpolationMode mode /* = InterpolationMode::Nearest */) {
+  return input.backend().getExtension<VisionExtension>().translate(
+      input, translation, outputDims, mode);
+}
+
+Tensor shear(
+    const Tensor& input,
+    const Shape& skews,
+    const Shape& outputDims /* = {} */,
+    const Tensor& fill /* = Tensor() */) {
+  return input.backend().getExtension<VisionExtension>().shear(
+      input, skews, outputDims, fill);
+}
+
+Tensor shear(
+    const Tensor& input,
+    const Shape& skews,
+    const Shape& outputDims /* = {} */,
+    const InterpolationMode mode /* = InterpolationMode::Nearest */) {
+  return input.backend().getExtension<VisionExtension>().shear(
+      input, skews, outputDims, mode);
+}
+
+Tensor gaussianFilter(const Shape& shape) {
+  // TODO{fl::Tensor} - empty tensor instantiation for defualt backend
+  return Tensor().backend().getExtension<VisionExtension>().gaussianFilter(
+      shape);
+}
+
+} // namespace fl
