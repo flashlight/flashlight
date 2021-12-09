b4f0393d96892418a5e98b91924ec9f55672b532
Jacob Kahn <jacobkahn@fb.com>
2021-12-08 16:07:42 -0800

    Add Vision TensorExtension
    Summary:
    Add a `TensorExtension` implemented for vision-specific functions. These are used in image transformations and other places, and designed to operate specifically on images.
    
    For now, only an `ArrayFireVisionExtension` is implemented using functions from `<af/image.h>`.
    
    Differential Revision: D32969019
    
    fbshipit-source-id: ceeaa51bc4a0eefd9ab9df38cd08b9b7bbbe6537

---
+++ flashlight/pkg/vision/tensor/VisionExtension.h
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "flashlight/fl/tensor/TensorBase.h"
+#include "flashlight/fl/tensor/TensorExtension.h"
+#include "flashlight/pkg/vision/tensor/VisionOps.h"
+
+namespace fl {
+
+// TODO: rename this file to VisionExtension
+class VisionExtension : public TensorExtension<VisionExtension> {
+ public:
+  static constexpr TensorExtensionType extensionType =
+      TensorExtensionType::Vision;
+
+  VisionExtension() = default;
+  virtual ~VisionExtension() = default;
+
+  virtual Tensor histogram(
+      const Tensor& tensor,
+      const unsigned numBins,
+      const double minVal,
+      const double maxVal) = 0;
+  virtual Tensor histogram(const Tensor& tensor, const unsigned numBins) = 0;
+
+  virtual Tensor equalize(const Tensor& input, const Tensor& histogram) = 0;
+
+  virtual Tensor resize(
+      const Tensor& tensor,
+      const Shape& shape,
+      const InterpolationMode mode) = 0;
+
+  virtual Tensor
+  rotate(const Tensor& input, const float theta, const Tensor& fill) = 0;
+  virtual Tensor rotate(
+      const Tensor& input,
+      const float theta,
+      const InterpolationMode mode) = 0;
+
+  virtual Tensor translate(
+      const Tensor& input,
+      const Shape& translation,
+      const Shape& outputDims,
+      const Tensor& fill) = 0;
+  virtual Tensor translate(
+      const Tensor& input,
+      const Shape& translation,
+      const Shape& outputDims,
+      const InterpolationMode mode) = 0;
+
+  virtual Tensor shear(
+      const Tensor& input,
+      const std::vector<float>& skews,
+      const Shape& outputDims,
+      const Tensor& fill) = 0;
+  virtual Tensor shear(
+      const Tensor& input,
+      const std::vector<float>& skews,
+      const Shape& outputDims,
+      const InterpolationMode mode) = 0;
+
+  virtual Tensor gaussianFilter(const Shape& shape) = 0;
+};
+
+} // namespace fl
