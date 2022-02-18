65978a69579cec9346f78648a4fc84f8b08c82e0
Jacob Kahn <jacobkahn@fb.com>
2022-02-18 10:47:20 -0800

    Fix arith ops for broadcasting tests, make AF type maps static (#820)
    Summary:
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/820
    
    - Fix a static init fiasco -- found this in CPU tests where we don't have huge NVIDIA drive init overhead
    - Fix arith errors with `% 0` -- ensure broadcast test input tensors can't have zeros
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D33977793
    
    fbshipit-source-id: ffdc00dcaf82a6ad631853fe61d01ffa437d4ccf

--- flashlight/fl/tensor/backend/af/Utils.cpp
+++ flashlight/fl/tensor/backend/af/Utils.cpp
@@ -16,37 +16,37 @@
 namespace fl {
 namespace detail {
 
-const std::unordered_map<fl::dtype, af::dtype> kFlashlightTypeToArrayFire = {
-    {fl::dtype::f16, af::dtype::f16},
-    {fl::dtype::f32, af::dtype::f32},
-    {fl::dtype::f64, af::dtype::f64},
-    {fl::dtype::b8, af::dtype::b8},
-    {fl::dtype::s16, af::dtype::s16},
-    {fl::dtype::s32, af::dtype::s32},
-    {fl::dtype::s64, af::dtype::s64},
-    {fl::dtype::u8, af::dtype::u8},
-    {fl::dtype::u16, af::dtype::u16},
-    {fl::dtype::u32, af::dtype::u32},
-    {fl::dtype::u64, af::dtype::u64}};
-
-const std::unordered_map<af::dtype, fl::dtype> kArrayFireTypeToFlashlight = {
-    {af::dtype::f16, fl::dtype::f16},
-    {af::dtype::f32, fl::dtype::f32},
-    {af::dtype::f64, fl::dtype::f64},
-    {af::dtype::b8, fl::dtype::b8},
-    {af::dtype::s16, fl::dtype::s16},
-    {af::dtype::s32, fl::dtype::s32},
-    {af::dtype::s64, fl::dtype::s64},
-    {af::dtype::u8, fl::dtype::u8},
-    {af::dtype::u16, fl::dtype::u16},
-    {af::dtype::u32, fl::dtype::u32},
-    {af::dtype::u64, fl::dtype::u64}};
-
 af::dtype flToAfType(fl::dtype type) {
+  static const std::unordered_map<fl::dtype, af::dtype>
+      kFlashlightTypeToArrayFire = {
+          {fl::dtype::f16, af::dtype::f16},
+          {fl::dtype::f32, af::dtype::f32},
+          {fl::dtype::f64, af::dtype::f64},
+          {fl::dtype::b8, af::dtype::b8},
+          {fl::dtype::s16, af::dtype::s16},
+          {fl::dtype::s32, af::dtype::s32},
+          {fl::dtype::s64, af::dtype::s64},
+          {fl::dtype::u8, af::dtype::u8},
+          {fl::dtype::u16, af::dtype::u16},
+          {fl::dtype::u32, af::dtype::u32},
+          {fl::dtype::u64, af::dtype::u64}};
   return kFlashlightTypeToArrayFire.at(type);
 }
 
 fl::dtype afToFlType(af::dtype type) {
+  static const std::unordered_map<af::dtype, fl::dtype>
+      kArrayFireTypeToFlashlight = {
+          {af::dtype::f16, fl::dtype::f16},
+          {af::dtype::f32, fl::dtype::f32},
+          {af::dtype::f64, fl::dtype::f64},
+          {af::dtype::b8, fl::dtype::b8},
+          {af::dtype::s16, fl::dtype::s16},
+          {af::dtype::s32, fl::dtype::s32},
+          {af::dtype::s64, fl::dtype::s64},
+          {af::dtype::u8, fl::dtype::u8},
+          {af::dtype::u16, fl::dtype::u16},
+          {af::dtype::u32, fl::dtype::u32},
+          {af::dtype::u64, fl::dtype::u64}};
   return kArrayFireTypeToFlashlight.at(type);
 }
 
