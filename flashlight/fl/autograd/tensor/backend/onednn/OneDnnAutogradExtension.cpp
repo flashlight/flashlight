53756fdfdea51ff95b074c90ee9f878a79457d3f
Jacob Kahn <jacobkahn@fb.com>
2022-02-26 15:30:49 -0800

    Migrate oneDNN backend to use AutogradExtension, add AutogradPayload
    Differential Revision: D33809314
    
    fbshipit-source-id: 3ae66938bc92b4fc4b8d0fff0a8d750ecbdeb7ee

---
+++ flashlight/fl/autograd/tensor/backend/onednn/OneDnnAutogradExtension.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/fl/autograd/tensor/backend/onednn/OneDnnAutogradExtension.h"
+
+namespace fl {
+
+bool OneDnnAutogradExtension::isDataTypeSupported(
+    const fl::dtype& dtype) const {
+  // fp16 computation is not supported with onednn
+  return dtype != fl::dtype::f16;
+}
+
+} // namespace fl
