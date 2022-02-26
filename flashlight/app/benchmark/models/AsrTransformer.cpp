8fe694b4912a56161427a4cb2483ebad96358dc5
Jacob Kahn <jacobkahn@fb.com>
2022-02-26 15:30:49 -0800

    Change app/benchmark to use fl::Tensor
    Differential Revision: D33521745
    
    fbshipit-source-id: c8490c9a53ad3cd698c314ef2afaaa1f76c71659

--- flashlight/app/benchmark/models/AsrTransformer.cpp
+++ flashlight/app/benchmark/models/AsrTransformer.cpp
@@ -13,7 +13,7 @@ namespace benchmark {
 
 AsrTransformer::AsrTransformer(int64_t nFeature, int64_t nLabel) {
   double dropout = 0.4;
-  convFrontend_->add(std::make_shared<fl::View>(af::dim4(-1, 1, nFeature, 0)));
+  convFrontend_->add(std::make_shared<fl::View>(Shape({-1, 1, nFeature, 0})));
   // Time x 1 x nFeature x Batch
   std::vector<int> lnDims = {0, 1, 2};
   convFrontend_->add(std::make_shared<fl::LayerNorm>(lnDims));
@@ -21,7 +21,7 @@ AsrTransformer::AsrTransformer(int64_t nFeature, int64_t nLabel) {
       std::make_shared<fl::Conv2D>(nFeature, 1536, 7, 1, 3, 1, -1, 0, 1, 1));
   convFrontend_->add(std::make_shared<fl::GatedLinearUnit>(2));
   convFrontend_->add(std::make_shared<fl::Dropout>(0.3));
-  convFrontend_->add(std::make_shared<fl::Reorder>(2, 0, 3, 1));
+  convFrontend_->add(std::make_shared<fl::Reorder>(Shape({2, 0, 3, 1})));
   // nFeature x Time x Batch x 1
   add(convFrontend_);
   sinpos_ = std::make_shared<fl::SinusoidalPositionEmbedding>(768, 1.0);
@@ -39,13 +39,14 @@ AsrTransformer::AsrTransformer(int64_t nFeature, int64_t nLabel) {
 std::vector<fl::Variable> AsrTransformer::forward(
     const std::vector<fl::Variable>& input) {
   auto out = input[0];
-  auto xSizes = input[1].array();
+  auto xSizes = input[1].tensor();
   // expected input dims T x C x 1 x B
   int T = out.dims(0), B = out.dims(3);
-  auto inputMaxSize = af::tile(af::max(xSizes), 1, B);
-  af::array inputNotPaddedSize = af::ceil(xSizes * T / inputMaxSize);
-  auto padMask = af::iota(af::dim4(T, 1), af::dim4(1, B)) <
-      af::tile(inputNotPaddedSize, T, 1);
+  // TODO{fl::Tensor} - check first non-singleton dimension
+  auto inputMaxSize = fl::tile(fl::amax(xSizes, {0}), {1, B});
+  Tensor inputNotPaddedSize = fl::ceil(xSizes * T / inputMaxSize);
+  auto padMask =
+      fl::iota({T, 1}, {1, B}) < fl::tile(inputNotPaddedSize, {T, 1});
   out = convFrontend_->forward(out);
   out = sinpos_->forward({out}).front();
   for (int trIdx = 0; trIdx < 36; trIdx++) {
