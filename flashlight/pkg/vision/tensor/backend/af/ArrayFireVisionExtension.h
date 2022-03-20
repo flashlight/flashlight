3b0a3fb27c9670c3fa78e5796ef477e1621651f3
Jacob Kahn <jacobkahn@fb.com>
2022-03-20 14:01:18 -0700

    Add Vision TensorExtension (#782)
    Summary:
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/782
    
    Add a `TensorExtension` implemented for vision-specific functions. These are used in image transformations and other places, and designed to operate specifically on images.
    
    For now, only an `ArrayFireVisionExtension` is implemented using functions from `<af/image.h>`.
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D32969019
    
    fbshipit-source-id: 06c888c57e018ba204b5264a8e66a5339b919541

---
+++ flashlight/pkg/vision/tensor/backend/af/ArrayFireVisionExtension.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "flashlight/pkg/vision/tensor/VisionExtension.h"
+
+#include <af/image.h>
+
+namespace fl {
+namespace detail {
+
+/*
+ * Convert a Flashlight interpolation mode into an ArrayFire interpolation mode
+ */
+constexpr af_interp_type flToAfInterpType(InterpolationMode mode);
+
+} // namespace detail
+
+class ArrayFireVisionExtension : public VisionExtension {
+ public:
+  static bool registered;
+
+  bool isDataTypeSupported(const fl::dtype& dtype) const override;
+
+  Tensor histogram(
+      const Tensor& tensor,
+      const unsigned numBins,
+      const double minVal,
+      const double maxVal) override;
+  Tensor histogram(const Tensor& tensor, const unsigned numBins) override;
+
+  Tensor equalize(const Tensor& input, const Tensor& histogram) override;
+
+  Tensor resize(
+      const Tensor& tensor,
+      const Shape& shape,
+      const InterpolationMode mode) override;
+
+  Tensor rotate(const Tensor& input, const float theta, const Tensor& fill)
+      override;
+  Tensor rotate(
+      const Tensor& input,
+      const float theta,
+      const InterpolationMode mode) override;
+
+  Tensor translate(
+      const Tensor& input,
+      const Shape& translation,
+      const Shape& outputDims,
+      const Tensor& fill) override;
+  Tensor translate(
+      const Tensor& input,
+      const Shape& translation,
+      const Shape& outputDims,
+      const InterpolationMode mode) override;
+
+  Tensor shear(
+      const Tensor& input,
+      const std::vector<float>& skews,
+      const Shape& outputDims,
+      const Tensor& fill) override;
+  Tensor shear(
+      const Tensor& input,
+      const std::vector<float>& skews,
+      const Shape& outputDims,
+      const InterpolationMode mode) override;
+
+  Tensor gaussianFilter(const Shape& shape) override;
+};
+
+} // namespace fl
