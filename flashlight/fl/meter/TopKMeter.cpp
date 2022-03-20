2d7c10f5e476367eea1d7fee5c5e274d322d2074
Jacob Kahn <jacobkahn@fb.com>
2022-03-20 14:01:18 -0700

    Change fl/meter to use fl::Tensor (#787)
    Summary: Pull Request resolved: https://github.com/flashlight/flashlight/pull/787
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D30182923
    
    fbshipit-source-id: 5753f6408fd33efbe492592be4149cf99195e867

--- flashlight/fl/meter/TopKMeter.cpp
+++ flashlight/fl/meter/TopKMeter.cpp
@@ -6,31 +6,30 @@
  */
 #include "flashlight/fl/meter/TopKMeter.h"
 
-#include <iostream>
 #include <stdexcept>
+
+#include "flashlight/fl/tensor/TensorBase.h"
 
 namespace fl {
 
-TopKMeter::TopKMeter(const int k)
-    : k_(k), correct_(0), n_(0) {};
+TopKMeter::TopKMeter(const int k) : k_(k), correct_(0), n_(0){};
 
-void TopKMeter::add(const af::array& output, const af::array& target) {
-  if (output.dims(1) != target.dims(0)) {
+void TopKMeter::add(const Tensor& output, const Tensor& target) {
+  if (output.dim(1) != target.dim(0)) {
     throw std::invalid_argument("dimension mismatch in TopKMeter");
   }
-  if (target.numdims() != 1) {
+  if (target.ndim() != 1) {
     throw std::invalid_argument(
         "output/target must be 1-dimensional for TopKMeter");
   }
 
-  af::array maxVals, maxIds, match;
+  Tensor maxVals, maxIds, match;
   topk(maxVals, maxIds, output, k_, 0);
-  match = af::batchFunc(
-      maxIds, af::moddims(target, {1, target.dims(0), 1, 1}), af::operator==);
-  const af::array correct = af::anyTrue(match, 0);
+  match = maxIds == fl::reshape(target, {1, target.dim(0), 1, 1});
+  const Tensor correct = fl::any(match, {0});
 
-  correct_ += af::count<int32_t>(correct);
-  const int batchsize = target.dims(0);
+  correct_ += fl::countNonzero(correct).scalar<int32_t>();
+  const int batchsize = target.dim(0);
   n_ += batchsize;
 }
 
