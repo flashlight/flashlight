18c34b0e70d29a573e679e1ccf219c32e0fdf3ef
Jacob Kahn <jacobkahn@fb.com>
2022-04-25 13:34:11 -0700

    Migrate fl/common to use fl::Tensor (#793)
    Summary: Pull Request resolved: https://github.com/flashlight/flashlight/pull/793
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D30524834
    
    fbshipit-source-id: c03e9fbdd8de9613e4819f465a098687a2bac96a

--- flashlight/fl/common/Types.h
+++ flashlight/fl/common/Types.h
@@ -8,9 +8,9 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 #include <unordered_set>
 
-#include "flashlight/fl/common/CppBackports.h"
 #include "flashlight/fl/common/Defines.h"
 
 namespace fl {
@@ -20,32 +20,31 @@ namespace detail {
 /**
  * Precision specifications for autograd operators based on optimization level.
  */
-const fl::cpp::
-    fl_unordered_map<OptimLevel, fl::cpp::fl_unordered_set<std::string>>
-        kOptimLevelTypeExclusionMappings = {
-            {OptimLevel::DEFAULT, {}}, // unused
-            {OptimLevel::O1,
-             // Perform all operations in fp16 except for:
-             {"batchnorm",
-              "reciprocal",
-              "erf",
-              "exp",
-              "log",
-              "log1p",
-              "pow",
-              "sum",
-              "mean",
-              "var",
-              "norm",
-              "normalize",
-              "softmax",
-              "logSoftmax",
-              "categoricalCrossEntropy",
-              "gelu"}},
-            {OptimLevel::O2,
-             // Perform all operations in fp16 except for:
-             {"batchnorm"}},
-            {OptimLevel::O3, {}} // Perform all operations in f16
+const std::unordered_map<OptimLevel, std::unordered_set<std::string>>
+    kOptimLevelTypeExclusionMappings = {
+        {OptimLevel::DEFAULT, {}}, // unused
+        {OptimLevel::O1,
+         // Perform all operations in fp16 except for:
+         {"batchnorm",
+          "reciprocal",
+          "erf",
+          "exp",
+          "log",
+          "log1p",
+          "pow",
+          "sum",
+          "mean",
+          "var",
+          "norm",
+          "normalize",
+          "softmax",
+          "logSoftmax",
+          "categoricalCrossEntropy",
+          "gelu"}},
+        {OptimLevel::O2,
+         // Perform all operations in fp16 except for:
+         {"batchnorm"}},
+        {OptimLevel::O3, {}} // Perform all operations in f16
 };
 
 } // namespace detail
