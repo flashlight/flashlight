21c8bb5dde55496fad6aa0971fda97ea8df985ea
Jacob Kahn <jacobkahn@fb.com>
2022-04-25 13:34:23 -0700

    Change fl/optim to use fl::Tensor (#796)
    Summary: Pull Request resolved: https://github.com/flashlight/flashlight/pull/796
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D30524855
    
    fbshipit-source-id: 5cdcae57066089cd76cda238388911fb473f78af

--- flashlight/fl/optim/Utils.cpp
+++ flashlight/fl/optim/Utils.cpp
@@ -7,6 +7,10 @@
 
 #include "flashlight/fl/optim/Utils.h"
 
+#include <cmath>
+
+#include "flashlight/fl/tensor/TensorBase.h"
+
 namespace fl {
 
 double clipGradNorm(const std::vector<Variable>& parameters, double maxNorm) {
@@ -15,8 +19,8 @@ double clipGradNorm(const std::vector<Variable>& parameters, double maxNorm) {
     if (!p.isGradAvailable()) {
       continue;
     }
-    const auto& grad = p.grad().array();
-    gradNorm += af::sum<double>(grad * grad);
+    const auto& grad = p.grad().tensor();
+    gradNorm += fl::sum(grad * grad).asScalar<double>();
   }
   gradNorm = std::sqrt(gradNorm);
   double scale = maxNorm / (gradNorm + 1e-6);
@@ -27,7 +31,7 @@ double clipGradNorm(const std::vector<Variable>& parameters, double maxNorm) {
     if (!p.isGradAvailable()) {
       continue;
     }
-    p.grad().array() *= scale;
+    p.grad().tensor() *= scale;
   }
   return gradNorm;
 }
