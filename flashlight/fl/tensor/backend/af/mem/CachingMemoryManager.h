128e73a2582a7ed0a8810cf0ddb5d72cac9fe87a
Jacob Kahn <jacobkahn@fb.com>
2022-05-02 17:15:52 -0700

    Move memory manager logging utils to compute API
    Summary:
    Remove the dependency of `fl/tensor/backend/af/mem` on old things from `fl/common` and add memory manager logging to the `TensorBackend` API.
    
    This can and should be cleaned up in later improvements to the memory management API to streamline how logging is surfaced and streamed out.
    
    Differential Revision: D33309219
    
    fbshipit-source-id: c602ede5d5c430ad67c2a76096822f7d4ba727c4

--- flashlight/fl/tensor/backend/af/mem/CachingMemoryManager.h
+++ flashlight/fl/tensor/backend/af/mem/CachingMemoryManager.h
@@ -12,6 +12,7 @@
 #include <memory>
 #include <mutex>
 #include <numeric>
+#include <ostream>
 #include <set>
 #include <unordered_map>
 #include <vector>
@@ -42,7 +43,10 @@ class CachingMemoryManager : public MemoryManagerAdapter {
       const unsigned elSize) override;
   size_t allocated(void* ptr) override;
   void unlock(void* ptr, bool userLock) override;
-  void printInfo(const char* msg, const int device) override;
+  void printInfo(
+      const char* msg,
+      const int device,
+      std::ostream* ostream = &std::cout) override;
   void userLock(const void* ptr) override;
   void userUnlock(const void* ptr) override;
   bool isUserLocked(const void* ptr) override;
