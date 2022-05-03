9c37a53f325aadba166057b8fd514c9adea1d794
Jacob Kahn <jacobkahn@fb.com>
2022-05-02 18:59:27 -0700

    Add AutogradExtension for autograd tensor ops (#778)
    Summary:
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/778
    
    Adds an `AutogradExtension` based on `TensorExtension`. Implements operations commonly found in autograd-aware deep learning packages (i.e. cuDNN, oneDNN, miOpen, SYCL-standard things, etc) under a set of common interfaces that can be conditionally compiled.
    
    In particular, this also exposes autograd ops to the common Flashlight Tensor interface, such that ops like `conv2d` can be used on tensors which are not autograd-aware (are not `Variable`s).
    
    The ops that are not integrable into this interface include:
    - conv2d
    - pool2d
    - batchnorm
    - rnn
    
    (and `backward` variances thereof, computing gradients with respect to various inputs)
    
    For now, create a `CudnnAutogradExtension` — will add a `OnednnAutogradExtension` in a later change.
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D32611515
    
    fbshipit-source-id: 959c9b887e08b5fc06ef4e130c389ea234002fae

---
+++ flashlight/fl/autograd/tensor/backend/cudnn/CudnnAutogradExtension.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include "flashlight/fl/autograd/tensor/backend/cudnn/CudnnAutogradExtension.h"
+
+#include <cudnn.h>
+
+#include "flashlight/fl/common/DynamicBenchmark.h"
+
+namespace fl {
+
+std::shared_ptr<fl::DynamicBenchmark>
+CudnnAutogradExtension::createBenchmarkOptions() {
+  return std::make_shared<fl::DynamicBenchmark>(
+      std::make_shared<fl::DynamicBenchmarkOptions<KernelMode>>(
+          std::vector<KernelMode>(
+              {KernelMode::F32,
+               KernelMode::F32_ALLOW_CONVERSION,
+               KernelMode::F16}),
+          fl::kDynamicBenchmarkDefaultCount));
+}
+
+bool CudnnAutogradExtension::isDataTypeSupported(const fl::dtype& dtype) const {
+  switch (dtype) {
+    case fl::dtype::f16:
+    case fl::dtype::f32:
+    case fl::dtype::f64:
+      return true;
+    default:
+      return false;
+  }
+}
+
+} // namespace fl
