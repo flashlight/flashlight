dbbaa2d6b76b303164cf19b003a7b981ca229c41
Jacob Kahn <jacobkahn@fb.com>
2022-05-02 17:15:52 -0700

    Change pkg/speech/data to use fl::Tensor (#790)
    Summary:
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/790
    
    Migrate `pkg/speech/data` to use `fl::Tensor`.
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D31984617
    
    fbshipit-source-id: 2059cd2d2d2d394d9f2452a45f50d56a9a48d35a

--- flashlight/pkg/speech/data/ListFileDataset.cpp
+++ flashlight/pkg/speech/data/ListFileDataset.cpp
@@ -66,46 +66,61 @@ int64_t ListFileDataset::size() const {
   return numRows_;
 }
 
-std::vector<af::array> ListFileDataset::get(const int64_t idx) const {
+std::vector<Tensor> ListFileDataset::get(const int64_t idx) const {
   checkIndexBounds(idx);
 
   auto audio = loadAudio(inputs_[idx]); // channels x time
-  af::array input;
+  Tensor input;
   if (inFeatFunc_) {
     input = inFeatFunc_(
-        static_cast<void*>(audio.first.data()), audio.second, af::dtype::f32);
+        static_cast<void*>(audio.first.data()), audio.second, fl::dtype::f32);
   } else {
-    input = af::array(audio.second, audio.first.data());
+    input = Tensor::fromBuffer(
+        {audio.second}, audio.first.data(), MemoryLocation::Host);
   }
 
-  af::array target;
+  Tensor target;
   if (tgtFeatFunc_) {
     std::vector<char> curTarget(targets_[idx].begin(), targets_[idx].end());
     target = tgtFeatFunc_(
         static_cast<void*>(curTarget.data()),
-        {static_cast<dim_t>(curTarget.size())},
-        af::dtype::b8);
+        {static_cast<Dim>(curTarget.size())},
+        fl::dtype::b8);
   }
   targetSizesCache_[idx] = target.elements();
 
-  af::array words;
+  Tensor words;
   if (wrdFeatFunc_) {
     std::vector<char> curTarget(targets_[idx].begin(), targets_[idx].end());
     words = wrdFeatFunc_(
         static_cast<void*>(curTarget.data()),
-        {static_cast<dim_t>(curTarget.size())},
-        af::dtype::b8);
+        {static_cast<Dim>(curTarget.size())},
+        fl::dtype::b8);
   }
 
-  af::array sampleIdx = af::array(ids_[idx].length(), ids_[idx].data());
-  af::array samplePath = af::array(inputs_[idx].length(), inputs_[idx].data());
-  af::array sampleDuration = af::array(1, inputSizes_.data() + idx);
-  af::array sampleTargetSize = af::constant(float(target.elements()), 1);
+  Tensor sampleIdx = Tensor::fromBuffer(
+      {static_cast<long long>(ids_[idx].length())},
+      const_cast<char*>(ids_[idx].data()), // fix me post C++-17?
+      MemoryLocation::Host);
+  Tensor samplePath = Tensor::fromBuffer(
+      {static_cast<long long>(inputs_[idx].length())},
+      inputs_[idx].data(),
+      MemoryLocation::Host);
+  Tensor sampleDuration =
+      Tensor::fromBuffer({1}, inputSizes_.data() + idx, MemoryLocation::Host);
+  Tensor sampleTargetSize = fl::full({1}, float(target.elements()));
 
-  return {input, target, words, sampleIdx, samplePath, sampleDuration, sampleTargetSize};
+  return {
+      input,
+      target,
+      words,
+      sampleIdx,
+      samplePath,
+      sampleDuration,
+      sampleTargetSize};
 }
 
-std::pair<std::vector<float>, af::dim4> ListFileDataset::loadAudio(
+std::pair<std::vector<float>, Shape> ListFileDataset::loadAudio(
     const std::string& handle) const {
   auto info = loadSoundInfo(handle.c_str());
   return {loadSound<float>(handle.c_str()), {info.channels, info.frames}};
@@ -127,8 +142,8 @@ int64_t ListFileDataset::getTargetSize(const int64_t idx) const {
   std::vector<char> curTarget(targets_[idx].begin(), targets_[idx].end());
   auto tgtSize = tgtFeatFunc_(
                      static_cast<void*>(curTarget.data()),
-                     {static_cast<dim_t>(curTarget.size())},
-                     af::dtype::b8)
+                     {static_cast<long long>(curTarget.size())},
+                     fl::dtype::b8)
                      .elements();
   targetSizesCache_[idx] = tgtSize;
   return tgtSize;
