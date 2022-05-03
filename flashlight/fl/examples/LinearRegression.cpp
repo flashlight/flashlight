e471fc2c9391d0cc181b1c1247e31521fd4bd74e
Jacob Kahn <jacobkahn@fb.com>
2022-05-02 18:59:27 -0700

    Migrate fl/examples to fl::Tensor (#735)
    Summary: Pull Request resolved: https://github.com/flashlight/flashlight/pull/735
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D30659988
    
    fbshipit-source-id: b8fb3c784e9d7f3819279cae35dedee8bdd13a54

--- flashlight/fl/examples/LinearRegression.cpp
+++ flashlight/fl/examples/LinearRegression.cpp
@@ -5,33 +5,37 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+#include <math.h>
 #include <iostream>
 
-#include "flashlight/fl/flashlight.h"
+#include "flashlight/fl/tensor/Init.h"
+#include "flashlight/fl/nn/nn.h"
+#include "flashlight/fl/tensor/Index.h"
+#include "flashlight/fl/tensor/Random.h"
+#include "flashlight/fl/tensor/TensorBase.h"
 
 int main() {
-  af::info();
   fl::init();
 
   // Create data
   const int nSamples = 10000;
   const int nFeat = 10;
-  auto X = af::randu(nFeat, nSamples) + 1; // X elements in [1, 2]
-  auto Y = /* signal */ af::sum(af::pow(X, 3), 0).T() +
-      /* noise */ af::sin(2 * M_PI * af::randu(nSamples));
+  auto X = fl::rand({nFeat, nSamples}) + 1; // X elements in [1, 2]
+  auto Y = /* signal */ fl::transpose(fl::sum(fl::power(X, 3), {0})) +
+      /* noise */ fl::sin(2 * M_PI * fl::rand({nSamples}));
 
   // Training params
   const int nEpochs = 100;
   const float learningRate = 0.001;
-  auto weight = fl::Variable(af::randu(1, nFeat), true /* isCalcGrad */);
-  auto bias = fl::Variable(af::constant(0.0, 1), true /* isCalcGrad */);
+  auto weight = fl::Variable(fl::rand({1, nFeat}), true /* isCalcGrad */);
+  auto bias = fl::Variable(fl::full({1}, 0.0), true /* isCalcGrad */);
 
   std::cout << "[Linear Regression] Started..." << std::endl;
 
   for (int e = 1; e <= nEpochs; ++e) {
-    af::array error = af::constant(0, 1);
+    fl::Tensor error = fl::full({1}, 0);
     for (int i = 0; i < nSamples; ++i) {
-      auto input = fl::Variable(X(af::span, i), false /* isCalcGrad */);
+      auto input = fl::Variable(X(fl::span, i), false /* isCalcGrad */);
       auto yPred = fl::matmul(weight, input) + bias;
 
       auto yTrue = fl::Variable(Y(i), false /* isCalcGrad */);
@@ -43,14 +47,14 @@ int main() {
       loss.backward();
 
       // Update the weight and bias
-      weight.array() = weight.array() - learningRate * weight.grad().array();
-      bias.array() = bias.array() - learningRate * bias.grad().array();
+      weight.tensor() = weight.tensor() - learningRate * weight.grad().tensor();
+      bias.tensor() = bias.tensor() - learningRate * bias.grad().tensor();
 
       // clear the gradients for next iteration
       weight.zeroGrad();
       bias.zeroGrad();
 
-      error += loss.array();
+      error += loss.tensor();
     }
 
     std::cout << "Epoch: " << e
