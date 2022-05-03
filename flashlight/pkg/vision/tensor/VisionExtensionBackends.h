206dbab6fb0e7cf3053b44dc9436ece4a9aeb0db
Jacob Kahn <jacobkahn@fb.com>
2022-05-02 17:15:52 -0700

    Add Vision TensorExtension (#782)
    Summary:
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/782
    
    Add a `TensorExtension` implemented for vision-specific functions. These are used in image transformations and other places, and designed to operate specifically on images.
    
    For now, only an `ArrayFireVisionExtension` is implemented using functions from `<af/image.h>`.
    
    Reviewed By: benoitsteiner
    
    Differential Revision: D32969019
    
    fbshipit-source-id: 37392838d5a713440b3385d8cac7e57375bb0b12

---
+++ flashlight/pkg/vision/tensor/VisionExtensionBackends.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include "flashlight/fl/tensor/TensorBackend.h"
+#include "flashlight/fl/tensor/TensorExtension.h"
+
+/*
+ * Conditionally include vision extensions
+ */
+#if FL_USE_ARRAYFIRE
+  #include "flashlight/pkg/vision/tensor/backend/af/ArrayFireVisionExtension.h"
+#endif
+
+namespace fl {
+
+/****************** Vision Extension Registration ******************/
+
+#if FL_USE_ARRAYFIRE
+FL_REGISTER_TENSOR_EXTENSION(
+    ArrayFireVisionExtension,
+    TensorBackendType::ArrayFire);
+#endif // FL_USE_ARRAYFIRE
+
+} // namespace fl
