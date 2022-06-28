237ff0b36c304b4b2e7c5edb4904e711b94b8070
Ryan Guo <ryanguo99@fb.com>
2022-06-27 20:38:33 -0700

    Add an ArrayFire-specialized CPU Stream
    Summary: As title, this is to accommodate ArrayFire's special `af::sync` on CPU
    
    Reviewed By: jacobkahn
    
    Differential Revision: D37421410
    
    fbshipit-source-id: 6be364811550a8d37b87e7e8c13393c580ba7cc6

---
+++ flashlight/fl/test/tensor/af/ArrayFireCPUStreamTest.cpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <gtest/gtest.h>
+
+#include "flashlight/fl/runtime/DeviceManager.h"
+#include "flashlight/fl/tensor/Init.h"
+#include "flashlight/fl/tensor/backend/af/ArrayFireCPUStream.h"
+
+using fl::DeviceManager;
+using fl::DeviceType;
+using fl::runtime::Stream;
+using fl::runtime::StreamType;
+using fl::ArrayFireCPUStream;
+
+TEST(ArrayFireCPUStreamTest, create) {
+  const auto& manager = DeviceManager::getInstance();
+  for (const auto x64Device : manager.getDevicesOfType(DeviceType::x64)) {
+    x64Device->setActive();
+    const auto afCpuStream = ArrayFireCPUStream::create();
+
+    ASSERT_EQ(afCpuStream->type, StreamType::Synchronous);
+    ASSERT_EQ(&afCpuStream->device(), x64Device);
+    ASSERT_EQ(&afCpuStream->impl<ArrayFireCPUStream>(), afCpuStream.get());
+  }
+}
+
+TEST(ArrayFireCPUStreamTest, relativeSync) {
+  const auto as1 = ArrayFireCPUStream::create();
+  const auto as2 = ArrayFireCPUStream::create();
+  const std::shared_ptr<Stream> s1 = as1;
+  const std::shared_ptr<Stream> s2 = as2;
+  ASSERT_NO_THROW(s1->relativeSync(*s2));
+  ASSERT_NO_THROW(s1->relativeSync(*as2));
+  ASSERT_NO_THROW(as1->relativeSync(*s2));
+  ASSERT_NO_THROW(as1->relativeSync(*as2));
+
+  const std::unordered_set<const Stream*> streams { s1.get(), s2.get() };
+  const std::shared_ptr<Stream> s3 = ArrayFireCPUStream::create();
+  ASSERT_NO_THROW(s3->relativeSync(streams));
+}
+
+TEST(ArrayFireCPUStreamTest, sync) {
+  const auto as1 = ArrayFireCPUStream::create();
+  ASSERT_NO_THROW(as1->sync().wait());
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  fl::init();
+  return RUN_ALL_TESTS();
+}
