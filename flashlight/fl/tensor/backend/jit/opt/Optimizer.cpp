ae156191343541d453591f1af1e0b3a4681b4f8f
Ryan Guo <ryanguo99@fb.com>
2022-08-05 11:47:39 -0700

    [JitBackend] Add JitOptimizerExtension and integrate it into Optimizer

--- flashlight/fl/tensor/backend/jit/opt/Optimizer.cpp
+++ flashlight/fl/tensor/backend/jit/opt/Optimizer.cpp
@@ -7,13 +7,36 @@
 
 #include "flashlight/fl/tensor/backend/jit/opt/Optimizer.h"
 
+#include <iterator>
+
+#include "flashlight/fl/tensor/TensorBackend.h"
+#include "flashlight/fl/tensor/backend/jit/opt/JitOptimizerExtension.h"
 #include "flashlight/fl/tensor/backend/jit/opt/passes/ScalarFolding.h"
 
 namespace fl {
 
-Optimizer::Optimizer() {
-  // TODO figure out a configuration API (e.g., LLVM pass style macro)
+namespace {
+
+template <typename T>
+void extend(std::vector<T>& extendee, std::vector<T>&& elems) {
+  extendee.insert(
+      std::end(extendee),
+      std::make_move_iterator(std::begin(elems)),
+      std::make_move_iterator(std::end(elems)));
+}
+
+} // namespace
+
+Optimizer::Optimizer(TensorBackend& backend) : backend_(backend) {
+  // TODO
+  // 1. figure out a configuration API (e.g., LLVM pass style macro)
+  // 2. think about ordering
   passes_.emplace_back(std::make_unique<ScalarFolding>());
+  auto& registrar = detail::TensorExtensionRegistrar::getInstance();
+  if (registrar.isTensorExtensionRegistered(
+          backend_.backendType(), TensorExtensionType::JitOptimizer)) {
+    extend(passes_, backend_.getExtension<JitOptimizerExtension>().passes());
+  }
 }
 
 Node* Optimizer::optimize(Node* node) {
