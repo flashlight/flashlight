77525138d019422094708bab2257663c3de53053
Jacob Kahn <jacobkahn@fb.com>
2022-08-04 18:59:48 -0700

    Add missing copyright header in TensorUnaryOpsTest
    Summary: Add missing copywrite header in TensorUnaryOpsTest -- flagged by OSS automated jobs
    
    Reviewed By: richjames0
    
    Differential Revision: D38377189
    
    fbshipit-source-id: 6358c657bb8d62996dfb58b06d48ddecfe9d5569

--- flashlight/fl/test/tensor/TensorUnaryOpsTest.cpp
+++ flashlight/fl/test/tensor/TensorUnaryOpsTest.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include <gtest/gtest.h>
 
 #include "flashlight/fl/tensor/Index.h"
