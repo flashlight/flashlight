dc7342fe5778fa971b4d5bc8c6b3585f6efc4af2
Ryan Guo <ryanguo99@fb.com>
2022-10-06 07:45:00 -0700

    Implement full (#1020)
    Summary:
    "Parse" `full` in JIT:).
    
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/1020
    
    Test Plan:
    Unit tests are added.
    
    ```
    make JitTensorTest
    ./flashlight/fl/test/JitTensorTest
    ```
    
    Reviewed By: bwasti
    
    Differential Revision: D40131532
    
    Pulled By: bwasti
    
    fbshipit-source-id: e068bd5b36eb3043f33b1dadddf430cf536fc75c

--- flashlight/fl/tensor/backend/jit/JitBackend.h
+++ flashlight/fl/tensor/backend/jit/JitBackend.h
@@ -21,6 +21,9 @@ class JitBackend : public TensorBackend {
   TensorBackend& wrappedBackend_;
   std::function<Tensor(Node*)> jitTensorCreator_;
 
+  template<typename T>
+  Tensor fullWithType(const Shape& shape, T value, dtype type);
+
  public:
   JitBackend(
       TensorBackend& wrappedBackend,
