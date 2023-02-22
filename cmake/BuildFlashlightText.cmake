b61baf6a4971340346ded02b4f4941c7f4093871
jacobkahn <jacobkahn1@gmail.com>
2023-02-22 12:38:19 -0800

    Update flashlight/text commit to use KenLM fetchcontent

--- cmake/BuildFlashlightText.cmake
+++ cmake/BuildFlashlightText.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
   flashlight-text
   GIT_REPOSITORY https://github.com/flashlight/text.git
-  GIT_TAG        bb7809bfb6399a2e94a92cb25c83d15e45aa386f # > v0.0.2
+  GIT_TAG        ebab3336632ff10d0639690b4f4b5dabaad443d6 # > v0.0.2
 )
 
 # KenLM support is required for Flashlight's speech package
