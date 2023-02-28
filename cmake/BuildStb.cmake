80f59a272babc4cb8a7fe2bf2fbe54ba603de7c7
jacobkahn <jacobkahn1@gmail.com>
2023-02-28 14:39:13 -0800

    Swap to CMake FetchContent and cleanup modules (#1060)
    Summary:
    See title. CMake `FetchContent` solves a bunch of install-related issues and makes reasoning about configurable downstream dependencies easier.
    
    Pull Request resolved: https://github.com/flashlight/flashlight/pull/1060
    
    Test Plan:
    CI +
    local builds without the required dependencies and with `FL_BUILD_STANDALONE` set to `ON`
    
    Reviewed By: bwasti
    
    Differential Revision: D41795910
    
    Pulled By: jacobkahn
    
    fbshipit-source-id: 63629941783f8aaffa13d8f297d0decb6feb24ea

--- cmake/BuildStb.cmake
+++ cmake/BuildStb.cmake
@@ -1,26 +1,16 @@
-include(ExternalProject)
+cmake_minimum_required(VERSION 3.16)
 
-set(stb_URL https://github.com/nothings/stb.git)
-set(stb_BUILD ${CMAKE_CURRENT_BINARY_DIR}/stb)
-set(stb_TAG b42009b3b9d4ca35bc703f5310eedc74f584be58)
+include(FetchContent)
 
-# Download stb
-ExternalProject_Add(
+FetchContent_Declare(
   stb
-  PREFIX stb
-  GIT_REPOSITORY ${stb_URL}
-  GIT_TAG ${stb_TAG}
-  BUILD_IN_SOURCE 0
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND ""
-  LOG_DOWNLOAD ON
-)
-
-ExternalProject_Get_Property(stb SOURCE_DIR)
-set(stb_SOURCE_DIR ${SOURCE_DIR})
-
-set(stb_INCLUDE_DIRS
-  $<BUILD_INTERFACE:${SOURCE_DIR}/>
-  $<INSTALL_INTERFACE:${stb_INSTALL_PATH}>
+  GIT_REPOSITORY https://github.com/nothings/stb.git
+  GIT_TAG        8b5f1f37b5b75829fc72d38e7b5d4bcbf8a26d55
   )
+
+FetchContent_GetProperties(stb)
+if(NOT stb_POPULATED)
+  FetchContent_Populate(stb)
+endif()
+
+set(stb_INCLUDE_DIRS ${stb_SOURCE_DIR})
